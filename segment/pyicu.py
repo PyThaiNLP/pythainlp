19a90a5c2a316ef6e5be112d46ae464c0635b508
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2016-06-24 00:18:00 +0700

    Create pyicu.py

---
+++ segment/pyicu.py
@@ -0,0 +1,27 @@
+import PyICU
+def isThai(chr):
+    cVal = ord(chr)
+    if(cVal >= 3584 and cVal <= 3711):
+        return True
+    return False
+
+
+def warp(txt):
+	bd = PyICU.BreakIterator.createWordInstance(PyICU.Locale("th"))
+	bd.setText(txt)
+	lastPos = bd.first()
+	retTxt = ""
+	try:
+		while(1):
+			currentPos = next(bd)
+			retTxt += txt[lastPos:currentPos]
+			# เฉพาะภาษาไทยเท่านั้น
+			if(isThai(txt[currentPos - 1])):
+				if(currentPos < len(txt)):
+					if(isThai(txt[currentPos])):
+                        # คั่นคำที่แบ่ง
+						retTxt += ','
+			lastPos = currentPos
+	except StopIteration:
+		pass
+	return txt  # (','.join(str(x) for x in txt))  # แยกออกมาเป็น list

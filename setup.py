ccb03c994eff8f1a90b069d7ce70f73c4f728d78
wannaphongcom <wannaphong@yahoo.com>
2016-06-23 22:10:56 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
 # ระบบติดตั้ง
+from distutils.core import setup
+
+setup(name='pythai-nlp',
+      version='0.1',
+      #description='Python Distribution Utilities',
+      author='Wannaphong',
+      author_email='wannaphong@yahoo.com',
+      #url='https://www.python.org/sigs/distutils-sig/',
+      #packages=['distutils', 'distutils.command'],
+     )

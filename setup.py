d491c92f3df59c4652011d82980fdb3080816b45
wannaphongcom <wannaphong@yahoo.com>
2016-06-23 22:24:27 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,10 @@ setup(name='pythai-nlp',
       author_email='wannaphong@yahoo.com',
       #url='https://www.python.org/sigs/distutils-sig/',
       #packages=['distutils', 'distutils.command'],
+      license='Apache License Version 2.0'
       classifiers=[
             'Development Status :: 3 - Alpha',
+            'License :: OSI Approved :: Apache Software License',
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.5',

98d6402811aaa169efaae67bdfdfecedf89afff2
wannaphongcom <wannaphong@yahoo.com>
2016-06-23 22:19:28 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,4 +8,11 @@ setup(name='pythai-nlp',
       author_email='wannaphong@yahoo.com',
       #url='https://www.python.org/sigs/distutils-sig/',
       #packages=['distutils', 'distutils.command'],
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.5',
+      ],
+
      )

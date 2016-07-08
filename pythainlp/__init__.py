d6b6f19ae5d786cd6b00ef7393b8374d65b96d82
Wannaphong <wannaphong@yahoo.com>
2016-07-08 23:57:39 +0700

    fix bug

--- pythainlp/__init__.py
+++ pythainlp/__init__.py
@@ -3,8 +3,8 @@
 __author__ = 'Wannaphong Phatthiyaphaibun'
 __email__ = 'wannaphong@yahoo.com'
 __version__ = '0.0.2'
-from pythainlp import romanization
-from pythainlp import segment
-from pythainlp import rank
-from pythainlp import change
-from pythainlp import number
+import pythainlp.romanization
+import pythainlp.segment
+import pythainlp.rank
+import pythainlp.change
+import pythainlp.number

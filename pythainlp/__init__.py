77561e0147028758388ee25bd514f6fc004ea02a
Wannaphong <wannaphong@yahoo.com>
2016-07-08 23:47:58 +0700

    fix bug import python < 3.4

--- pythainlp/__init__.py
+++ pythainlp/__init__.py
@@ -3,3 +3,8 @@
 __author__ = 'Wannaphong Phatthiyaphaibun'
 __email__ = 'wannaphong@yahoo.com'
 __version__ = '0.0.2'
+from pythainlp.romanization import romanization
+from pythainlp.segment import segment
+from pythainlp.rank import rank
+from pythainlp.change import *
+from pythainlp.number import *

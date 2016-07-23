eb61c00eb32989d5cee62370ffbd5564f7ce38e2
Wannaphong <wannaphong@yahoo.com>
2016-07-23 16:51:07 +0700

    add corpus

---
+++ pythainlp/corpus/thaiword.py
@@ -0,0 +1,11 @@
+import os
+import codecs
+import pythainlp
+templates_dir = os.path.join(os.path.dirname(pythainlp.__file__), 'corpus')
+template_file = os.path.join(templates_dir, 'thaiword.txt')
+def data():
+	with codecs.open(template_file, 'r',encoding='utf8') as f:
+		lines = f.read().splitlines()
+	return lines
+if __name__ == "__main__":
+	print(data())

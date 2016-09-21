927b814d16b3319c177ef47a641bdfef845cfdd0
Wannaphong <wannaphong@yahoo.com>
2016-09-21 22:04:23 +0700

    fix test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
 import unittest
-import pythainlp
+from pythainlp.segment import segment
 from pythainlp.rank import rank
-from pythainlp.romanization import romanization
 from pythainlp.change import *
 from pythainlp.number import numtowords
 from pythainlp.postaggers import tag
 class TestUM(unittest.TestCase):
-	def setUp(self):
-		pass
 	def segment_test(self):
-		self.assertEqual(pythainlp.segment.segment('ฉันรักภาษาไทยเพราะฉันเป็นคนไทย'))
+		self.assertEqual(segment('ฉันรักภาษาไทยเพราะฉันเป็นคนไทย'),['ฉัน', 'รัก', 'ภาษา', 'ไทย', 'เพราะ', 'ฉัน', 'เป็น', 'คน', 'ไทย'])
 	def rank_test(self):
-		self.assertEqual(pythainlp.rank.rank(["แมว","คน","แมว"]))
+		self.assertEqual(rank(["แมว","คน","แมว"]),Counter({'แมว': 2, 'คน': 1}))
 	def change_test(self):
-		self.assertEqual(pythainlp.change.texttothai("l;ylfu8iy["))
+		self.assertEqual(texttothai("l;ylfu8iy["),'สวัสดีครับ')
 	def number_test(self):
-		self.assertEqual(pythainlp.number.numtowords(5611116.50))
+		self.assertEqual(numtowords(5611116.50),'ห้าล้านหกแสนหนึ่งหมื่นหนึ่งพันหนึ่งร้อยสิบหกบาทห้าสิบสตางค์')
 	def tag_test(self):
-		self.assertEqual(pythainlp.postaggers.tag("คุณกำลังประชุม"))
-if __name__ == '__main__':
+		self.assertEqual(tag("คุณกำลังประชุม"),[('คุณ', 'PPRS'), ('กำลัง', 'XVBM'), ('ประชุม', 'VACT')])
+def main():
     unittest.main()
+if __name__ == '__main__':
+    main()

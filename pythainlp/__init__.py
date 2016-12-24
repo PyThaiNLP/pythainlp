363c8cc64b03b448ddab7c4d7c92a29529db6bde
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2016-12-24 22:17:57 +0700

    Update __init__.py

--- pythainlp/__init__.py
+++ pythainlp/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import
 __author__ = 'Wannaphong Phatthiyaphaibun'
 __email__ = 'wannaphong@yahoo.com'
-__version__ = '0.0.7'
+__version__ = '0.0.8'
 from . import romanization
 from . import segment
 from . import rank
@@ -11,3 +11,4 @@ from . import number
 from . import date
 from . import postaggers
 from . import collation
+from . import spell

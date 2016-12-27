d00dd77b49b5b7d9a6168d12fa72c26200dc75e9
Wannaphong <wannaphong@yahoo.com>
2016-12-27 20:41:58 +0700

    fix hunspell

--- docs/install.md
+++ docs/install.md
@@ -3,15 +3,21 @@
 สามารถติดตั้งได้โดยใช้คำสั่ง
 
 รุ่นเสถียร
+
 ```sh
 $ pip3 install pythainlp
 ```
 รุ่นกำลังพัฒนา
+
 ```sh
 $ git clone https://github.com/wannaphongcom/pythainlp.git
 $ cd pythainlp
 $ python setup.py install
 ```
+
+# สำหรับเปิดใช้งานระบบเช็คคำผิด
+
+ต้องทำการติดตั้ง hunspell ให้เรียบร้อย
 
 ## สำหรับ Windows
 
--- pythainlp/spell/hunspell.py
+++ pythainlp/spell/hunspell.py
@@ -4,25 +4,29 @@ import subprocess
 import sys
 
 def spell(word,lang):
-	if sys.platform == 'win32':
-		cmd = "echo "+word+" | hunspell -d "+lang
-	else:
-		cmd = 'echo "'+word+'" | hunspell -d '+lang
-	getoutput = subprocess.getoutput(cmd)
-	del cmd
-	get = getoutput.split("\n")
-	del get[0]
-	if get[0] == '*':
-		getoutput = "No Suggestions"
-	else:
-		if get[1] == "":
-			del get[1]
-		get = get[0].split(":")
+	try:
+		if sys.platform == 'win32':
+			cmd = "echo "+word+" | hunspell -d "+lang
+		else:
+			cmd = 'echo "'+word+'" | hunspell -d '+lang
+		getoutput = subprocess.getoutput(cmd)
+		del cmd
+		get = getoutput.split("\n")
 		del get[0]
-		getoutput = get[0].replace(" ","")
-		getoutput = getoutput.split(",")
-	del get
-	return getoutput
+		if get[0] == '*':
+			getoutput = "No Suggestions"
+		else:
+			if get[1] == "":
+				del get[1]
+			get = get[0].split(":")
+			del get[0]
+			getoutput = get[0].replace(" ","")
+			getoutput = getoutput.split(",")
+		del get
+		return getoutput
+	except:
+		print('plase install hunspell')
+		return None
 if __name__ == "__main__":
   Input = spell("appoe","")
   print(Input)

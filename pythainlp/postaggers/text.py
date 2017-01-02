3c2a426553072149710d976938918d837cd6c9ff
Wannaphong <wannaphong@yahoo.com>
2017-01-02 23:32:29 +0700

    fix pos

--- pythainlp/postaggers/text.py
+++ pythainlp/postaggers/text.py
@@ -3,14 +3,13 @@ from nine import nimport,str
 from pythainlp.segment import segment
 import pythainlp
 import os
-json= nimport('json')
 import nltk.tag, nltk.data
+json= nimport('json')
+codecs= nimport('codecs')
+reader = codecs.getreader("utf-8")
 templates_dir = os.path.join(os.path.dirname(pythainlp.__file__), 'corpus')
 template_file = os.path.join(templates_dir, 'thaipos.json')
-#default_tagger = nltk.data.load(nltk.tag._POS_TAGGER)
-def data():
-	return json.load(open(template_file).read())
-data1 =data()
+data1 = json.load(reader(open(template_file).read()))
 #Postaggers ภาษาไทย
 def tag(text):
 	"""รับค่าเป็นข้อความ ''str'' คืนค่าเป็น ''list'' เช่น [('ข้อความ', 'ชนิดคำ')]"""

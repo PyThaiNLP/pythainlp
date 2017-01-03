a1b2c545f6055d48cee74e7e6ece731ad465be8d
Wannaphong <wannaphong@yahoo.com>
2017-01-03 12:49:28 +0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -51,8 +51,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ]
+        'Programming Language :: Python :: 3.5']
     #scripts = ['test.py']
     #tests_require=test_requirements
 )

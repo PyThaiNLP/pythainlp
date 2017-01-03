6b1bd07a0c30cd40d3eea82b9f66b7f75c33f4b1
Wannaphong <wannaphong@yahoo.com>
2017-01-03 11:46:40 +0700

    fix import future

--- pythainlp/corpus/__init__.py
+++ pythainlp/corpus/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import,print_function
 __all__ = ["thaipos", "thaiword"]
-from __future__ import absolute_import
-from __future__ import print_function
 from .thaipos import get_data
 from .thaiword import get_data
--- pythainlp/spell/__init__.py
+++ pythainlp/spell/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-__all__ = ["hunspell"]
 from __future__ import absolute_import
+__all__ = ["hunspell"]
 from .hunspell import spell

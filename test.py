a009dabd539f00999553625b2c7f3a834d46a7a7
Wannaphong <wannaphong@yahoo.com>
2017-01-04 22:03:45 +0700

    add more test

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_aliases()
 import unittest
 import optparse
 from collections import Counter
@@ -7,11 +13,16 @@ from pythainlp.rank import rank
 from pythainlp.change import *
 from pythainlp.number import numtowords
 from pythainlp.postaggers import tag
+from pythainlp.romanization import romanization
 class TestUM(unittest.TestCase):
 	def testSegment(self):
 		self.assertEqual(segment('ฉันรักภาษาไทยเพราะฉันเป็นคนไทย'),['ฉัน', 'รัก', 'ภาษา', 'ไทย', 'เพราะ', 'ฉัน', 'เป็น', 'คน', 'ไทย'])
+	def testRank(self):
+		self.assertEqual(rank(["แมว","คน","แมว"]),Counter({'แมว': 2, 'คน': 1}))
 	def testChange(self):
 		self.assertEqual(texttothai("l;ylfu8iy["),'สวัสดีครับ')
+	def testRomanization(self):
+		self.assertEqual(romanization("แมว"),'mæw')
 	def testNumber(self):
 		self.assertEqual(numtowords(5611116.50),'ห้าล้านหกแสนหนึ่งหมื่นหนึ่งพันหนึ่งร้อยสิบหกบาทห้าสิบสตางค์')
 	def testTag(self):

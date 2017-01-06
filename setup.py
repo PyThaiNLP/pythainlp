08147e9e84cd99c244bfa950f7c7bcbe920ae622
Wannaphong <wannaphong@yahoo.com>
2017-01-06 20:28:08 +0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -48,6 +48,5 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
 	]
 )

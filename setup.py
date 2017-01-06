59dba35483c651f2708a6724c2730381f25fdb14
Wannaphong <wannaphong@yahoo.com>
2017-01-06 20:59:40 +0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 
 from setuptools import setup
 from setuptools import find_packages
+import sys
 import codecs
 
 with codecs.open('README.rst','r',encoding='utf-8') as readme_file:
@@ -19,31 +20,49 @@ requirements = [
 test_requirements = [
     # TODO: put package test requirements here
 ]
-
-setup(
-    name='pythainlp',
-    version='1.0.0',
-    description="Thai NLP in python package.",
-    long_description=readme,
-    author='Wannaphong Phatthiyaphaibun',
-    author_email='wannaphong@yahoo.com',
-    url='https://github.com/wannaphongcom/pythainlp',
-    packages=find_packages(),
-    test_suite='test',
-    package_data={'pythainlp.corpus':['thaipos.json','thaiword.txt']},
-    include_package_data=True,
-    install_requires=requirements,
-    license='Apache Software License 2.0',
-    zip_safe=False,
-    keywords='pythainlp',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: Thai',
-        'Topic :: Text Processing :: Linguistic',
-        'Programming Language :: Python :: Implementation',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        ],
-)
+if sys.version_info > (3,3):
+	setup(
+		name='pythainlp',
+		version='1.0.0',
+		description="Thai NLP in python package.",
+		long_description=readme,
+		author='Wannaphong Phatthiyaphaibun',
+		author_email='wannaphong@yahoo.com',
+		url='https://github.com/wannaphongcom/pythainlp',
+		packages=find_packages(),
+		test_suite='test',
+		package_data={'pythainlp.corpus':['thaipos.json','thaiword.txt']},
+		include_package_data=True,
+		install_requires=requirements,
+		license='Apache Software License 2.0',
+		zip_safe=False,
+		keywords='pythainlp',
+		classifiers=[
+			'Development Status :: 5 - Production/Stable',
+			'Intended Audience :: Developers',
+			'License :: OSI Approved :: Apache Software License',
+			'Natural Language :: Thai',
+			'Topic :: Text Processing :: Linguistic',
+			'Programming Language :: Python :: Implementation',
+			'Programming Language :: Python :: 2.7',
+			'Programming Language :: Python :: 3',
+			],
+	)
+else:
+	setup(
+		name='pythainlp',
+		version='1.0.0',
+		description="Thai NLP in python package.",
+		long_description=readme,
+		author='Wannaphong Phatthiyaphaibun',
+		author_email='wannaphong@yahoo.com',
+		url='https://github.com/wannaphongcom/pythainlp',
+		packages=find_packages(),
+		test_suite='test',
+		package_data={'pythainlp.corpus':['thaipos.json','thaiword.txt']},
+		include_package_data=True,
+		install_requires=requirements,
+		license='Apache Software License 2.0',
+		zip_safe=False,
+		keywords='pythainlp',
+	)

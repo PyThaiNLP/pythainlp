922f8ea3e0b2a99d06a035feb81d4c738edf7000
Wannaphong <wannaphong@yahoo.com>
2017-01-08 15:13:13 +0700

    add country

--- pythainlp/corpus/__init__.py
+++ pythainlp/corpus/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
-__all__ = ["thaipos", "thaiword","alphabet","tone"]
+__all__ = ["thaipos", "thaiword","alphabet","tone","country"]
 from .thaipos import get_data
 from .thaiword import get_data
 from .alphabet import get_data
 from .tone import get_data
+from .country import get_data

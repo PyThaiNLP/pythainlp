c0cba513d1d54b9572c84132e7c15285a330468a
Wannaphong <wannaphong@yahoo.com>
2017-01-09 18:54:41 +0700

    แยก pyicu ออกมา

--- pythainlp/romanization/__init__.py
+++ pythainlp/romanization/__init__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import,unicode_literals
-import icu
-# ถอดเสียงภาษาไทยเป็น Latin
-def romanization(data):
-	"""เป็นคำสั่ง ถอดเสียงภาษาไทยเป็น Latin รับค่า ''str'' ข้อความ คืนค่าเป็น ''str'' ข้อความ Latin"""
-	thai2latin = icu.Transliterator.createInstance('Thai-Latin')
-	return thai2latin.transliterate(data)
+__all__ = ['pyicu']
+try:
+	from .pyicu import romanization
+except:
+	print("error")

354902217d2e4f4d99d16696585b7c3cbbcd0eef
Wannaphong <wannaphong@yahoo.com>
2017-03-27 10:34:30 +0700

    fix utf-8

--- pythainlp/corpus/alphabet.py
+++ pythainlp/corpus/alphabet.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import,unicode_literals
+﻿from __future__ import absolute_import,unicode_literals
 import six
 def get_data():
 	"""เป็นคำสั่งสำหรับดึงตัวอักษร ก - ฮ ในภาษาไทย

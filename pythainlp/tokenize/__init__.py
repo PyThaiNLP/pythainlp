3fd58dbebc2facb318aba3885c78f51b7730127f
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2017-05-18 22:38:56 +0700

    add 	api engine in pythainlp.tokenize.word_tokenize

--- pythainlp/tokenize/__init__.py
+++ pythainlp/tokenize/__init__.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 #  TODO ปรับ API ให้เหมือน nltk
-from __future__ import absolute_import,division,print_function,unicode_literals
-from pythainlp.segment import segment
-def word_tokenize(text):
+from __future__ import absolute_import,division,unicode_literals
+def word_tokenize(text,engine='icu'):
+	"""
+	ระบบตัดคำภาษาไทย
+
+	word_tokenize(text,engine='icu')
+	engine มี
+	- icu
+	- dict
+	"""
+	if engine=='icu':
+    		from pythainlp.segment.pyicu import segment
+	elif engine=='dict':
+    		from pythainlp.segment.dict import segment
 	return segment(text)

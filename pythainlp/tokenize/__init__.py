eb4c4c390539c07b94cb619aa2277827fdd5dcde
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2017-07-05 12:14:46 +0700

    add deepcut

--- pythainlp/tokenize/__init__.py
+++ pythainlp/tokenize/__init__.py
@@ -4,22 +4,44 @@ def word_tokenize(text,engine='icu'):
 	"""
 	ระบบตัดคำภาษาไทย
 
-	word_tokenize(text,engine='mm')
+	word_tokenize(text,engine='icu')
 	engine มี
-	- icu
-	- dict
-	- mm ใช้ Maximum Matching algorithm
-	- pylexto ใช้ LexTo ในการตัดคำ
+	- icu -  engine ตัวดั้งเดิมของ PyThaiNLP (ความแม่นยำต่ำ) และเป็นค่าเริ่มต้น
+	- dict - ใช้ dicu ในการตัดคำไทย จะคืนค่า False หากไม่สามารถตัดคำไทย
+	- mm ใช้ Maximum Matching algorithm - โค้ดชุดเก่า
 	- newmm - ใช้ Maximum Matching algorithm ในการตัดคำภาษาไทย โค้ดชุดใหม่
+	- pylexto ใช้ LexTo ในการตัดคำ
+	- deepcut ใช้ Deep Neural Network ในการตัดคำภาษาไทย
 	"""
 	if engine=='icu':
+    		'''
+			ตัดคำภาษาไทยโดยใช้ icu ในการตัดคำ
+			'''
     		from .pyicu import segment
 	elif engine=='dict':
+    		'''
+			ใช้ dicu ในการตัดคำไทย
+			จะคืนค่า False หากไม่สามารถตัดคำไทย
+			'''
     		from .dictsegment import segment
 	elif engine=='mm':
+    		'''
+			ใช้ Maximum Matching algorithm - โค้ดชุดเก่า
+			'''
     		from .mm import segment
-	elif engine=='pylexto':
-    		from .pylexto import segment
 	elif engine=='newmm':
+    		'''
+			ใช้ Maximum Matching algorithm ในการตัดคำภาษาไทย โค้ดชุดใหม่
+			'''
     		from .newmm import mmcut as segment
+	elif engine=='pylexto':
+    		'''
+			ใช้ LexTo ในการตัดคำ
+			'''
+    		from .pylexto import segment
+	elif engine=='deepcut':
+    		'''
+			ใช้ Deep Neural Network ในการตัดคำภาษาไทย
+			'''
+    		from .deepcut import segment
 	return segment(text)
---
+++ pythainlp/tokenize/deepcut.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import,unicode_literals
+import sys
+try:
+    import deepcut
+except ImportError:
+	'''
+    ในกรณ๊ที่ยังไม่ติดตั้ง deepcut ในระบบ
+    '''
+	import pip
+	pip.main(['install','deepcut'])
+	try:
+		from pylexto import LexTo
+	except ImportError:
+		sys.exit('Error ! using pip install deepcut')
+def segment(text):
+    return deepcut.tokenize(text)

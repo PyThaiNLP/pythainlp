a2e43beea01e429a51db0d63c8b5a541ef1c889b
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2017-07-11 19:45:18 +0700

    fix py2.7 #2

--- pythainlp/tokenize/newmm.py
+++ pythainlp/tokenize/newmm.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import,division,unicode_literals,print_function
 '''
 ตัดคำภาษาไทยโดยใช้ Maximum Matching algorithm
 เดติดโค้ดต้นฉบับ คุณ Korakot Chaovavanich

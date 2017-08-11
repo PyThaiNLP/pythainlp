1d4be994b4760425dbf2e359c6cf423b0fd51638
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2017-08-11 23:24:49 +0700

    update code newmm

--- pythainlp/tokenize/newmm.py
+++ pythainlp/tokenize/newmm.py
@@ -5,14 +5,17 @@ from builtins import *
 ตัดคำภาษาไทยโดยใช้ Maximum Matching algorithm
 เดติดโค้ดต้นฉบับ คุณ Korakot Chaovavanich
 จาก https://www.facebook.com/groups/408004796247683/permalink/431283740586455/ 
+และ https://gist.github.com/korakot/fe26c65dc9eed467f4497f784a805716
 '''
+import re
 from marisa_trie import Trie
 from collections import Counter, defaultdict
 from pythainlp.corpus.thaiword import get_data
+trie = Trie(get_data())
 class LatticeString(str):
+    ''' String subclass เพื่อเก็บวิธีตัดหลายๆ วิธี
+    '''
     def __new__(cls, value, multi=None, in_dict=True): 
-        ''' Return a string instance 
-        ''' 
         return str.__new__(cls, value)
     
     def __init__(self, value, multi=None, in_dict=True):
@@ -23,23 +26,19 @@ class LatticeString(str):
                 self.unique = False
         else:
             self.multi = [value]
-            
         self.in_dict = in_dict   # บอกว่าเป็นคำมีในดิกหรือเปล่า
+        
+spat_eng = r'''(?x)
+[-a-zA-Z]+|   # english
+\d[\d,\.]*|   # number
+[ \t]+|       # space
+\r?\n         # newline
+'''
+pat_eng = re.compile(spat_eng)
 
-    def suggest(self):
-        return []
-def serialize(p, p2):
-    for w in words_at[p]:
-        p_ = p + len(w)
-        if p_== p2:
-            yield w
-        elif p_ < p2:
-            for path in serialize(p_, p2):
-                yield w+'/'+path
-# มี jigsaw พร้อมแล้ว  ต่อไปก็ลองเขียน tcut ใหม่
-def tcut(text):
-    #global last_p, i, q, ww   # for debug
-    trie = Trie(get_data())
+def multicut(text):
+    ''' ส่งคืน LatticeString คืนมาเป็นก้อนๆ 
+    ''' 
     words_at = defaultdict(list)  # main data structure
     
     def serialize(p, p2):    # helper function
@@ -68,25 +67,28 @@ def tcut(text):
             
         # กรณี len(q) == 0  คือ ไม่มีใน dict
         if len(q)==0:
-            # skip น้อยที่สุด ที่เป็นไปได้
-            for i in range(p, len(text)):
-                ww = trie.prefixes(text[i:])
-                if ww:
-                    break
-            else:
-                i = len(text)
+            m = pat_eng.match(text[p:])
+            if m: # อังกฤษ, เลข, ว่าง
+                i = p + m.span()[1]
+            else: # skip น้อยที่สุด ที่เป็นไปได้
+                for i in range(p, len(text)):
+                    ww = trie.prefixes(text[i:])
+                    m = pat_eng.match(text[i:])
+                    if ww or m:
+                        break
+                else:
+                    i = len(text)
             w = text[p:i]
-            w = w.replace(' ','') # ลบค่าที่ว่าง
             words_at[p].append(w)
             yield LatticeString(w, in_dict=False)
             last_p = i
             q.add(i)
+            
 def mmcut(text):
-    '''
-	ใช้ Maximum Matching algorithm ในการตัดคำภาษาไทย
+    ''' Maximum Matching algorithm ในการตัดคำภาษาไทย
     '''
     res = []
-    for w in tcut(text):
+    for w in multicut(text):
         if w.unique:
             res.append(w)
         else:
@@ -109,9 +111,9 @@ def listcut(text):
     '''
 	ใช้ในการหา list ที่สามารถตัดคำได้ทั้งหมด
 	'''
-    ww = list(tcut(text))
+    ww = list(multicut(text))
     return list(combine(ww))
 if __name__ == "__main__":
 	text='ผมรักคุณนะครับโอเคบ่พวกเราเป็นคนไทยรักภาษาไทยภาษาบ้านเกิด'
 	print(mmcut(text))
-	#print(listcut(text))
+	print(listcut(text))

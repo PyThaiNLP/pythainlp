a82e9d584be503eb4e8ad1bc167fb2c4ac82bf3b
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2017-08-17 01:48:23 +0700

    del stopword from pythainlp.sentiment

--- pythainlp/sentiment/__init__.py
+++ pythainlp/sentiment/__init__.py
@@ -32,9 +32,9 @@ def sentiment(text):
 	with open(os.path.join(templates_dir, 'sentiment.data'), 'rb') as in_strm:
 		classifier = dill.load(in_strm)
 	in_strm.close()
-	text=word_tokenize(text)
+	text=set(word_tokenize(text))-set(stopwords.words('thai'))
 	featurized_test_sentence =  {i:(i in text) for i in vocabulary}
 	return classifier.classify(featurized_test_sentence)
 if __name__ == '__main__':
-	d="ดีใจ"
+	d="เสียใจแย่มากเลย"
 	print(sentiment(d))

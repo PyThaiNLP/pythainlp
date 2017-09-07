28469303d9f1f4634011b52c278a40e97991e41c
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2017-09-07 22:34:09 +0700

    add pythainlp.corpus.names

--- pythainlp/corpus/__init__.py
+++ pythainlp/corpus/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import,unicode_literals
+from nltk.corpus import names
 #__all__ = ["thaipos", "thaiword","alphabet","tone","country","wordnet"]

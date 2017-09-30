b02ef8acd214121e0fb511bbc28213422c779f2f
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2017-09-30 22:01:23 +0700

    add word tokenize in pythainlp.romanization.icu

--- pythainlp/romanization/pyicu.py
+++ pythainlp/romanization/pyicu.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import,unicode_literals
 import icu
+from pythainlp.tokenize import word_tokenize
 # ถอดเสียงภาษาไทยเป็น Latin
 def romanization(data):
 	"""เป็นคำสั่ง ถอดเสียงภาษาไทยเป็น Latin รับค่า ''str'' ข้อความ คืนค่าเป็น ''str'' ข้อความ Latin"""
+	word_list=word_tokenize(data)
 	thai2latin = icu.Transliterator.createInstance('Thai-Latin')
-	return thai2latin.transliterate(data)
+	listword=['']*len(word_list)
+	i=0
+	while i<len(word_list):
+		listword[i]=thai2latin.transliterate(word_list[i])
+		i+=1
+	return ''.join(listword)

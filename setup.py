527797f9a9eb085adeb2519357280912d938b90b
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2017-10-10 00:57:47 +0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -6,58 +6,58 @@ import codecs
 import platform
 import sys
 import struct
+def windows_is():
+    return platform.system()=='Windows'
+def icu1():
+    if windows_is()==True:
+        try:
+            import icu
+            i=True
+        except ImportError:
+            i=False
+        python_version='{0[0]}.{0[1]}'.format(sys.version_info)
+        bit=struct.calcsize("P") * 8
+        if python_version=="3.6" and i!=True:
+            if bit=="64":
+                icu='https://github.com/wannaphongcom/pythainlp-installer/raw/master/windows/pyicu/PyICU-cp36-cp36m-win_amd64.whl'
+            else:
+                icu='https://github.com/wannaphongcom/pythainlp-installer/raw/master/windows/pyicu/PyICU-cp36-cp36m-win32.whl'
+        elif python_version=="3.5" and i!=True:
+            if bit=="64":
+                icu='https://github.com/wannaphongcom/pythainlp-installer/raw/master/windows/pyicu/PyICU-cp35-cp35m-win_amd64.whl'
+            else:
+                icu='https://github.com/wannaphongcom/pythainlp-installer/raw/master/windows/pyicu/PyICU-cp35-cp35m-win32.whl'
+        elif python_version=="3.4" and i!=True:
+            if bit=="64":
+                icu='https://github.com/wannaphongcom/pythainlp-installer/raw/master/windows/pyicu/PyICU-cp34-cp34m-win_amd64.whl'
+            else:
+                icu='https://github.com/wannaphongcom/pythainlp-installer/raw/master/windows/pyicu/PyICU-cp34-cp34m-win32.whl'
+        elif python_version=="2.7" and i!=True:
+            if bit=="64":
+                icu='https://github.com/wannaphongcom/pythainlp-installer/raw/master/windows/pyicu/PyICU-cp27-cp27m-win_amd64.whl'
+            else:
+                icu='https://github.com/wannaphongcom/pythainlp-installer/raw/master/windows/pyicu/PyICU-cp27-cp27m-win32.whl'
+        else:
+            icu='pyicu'
+    else:
+        icu='pyicu'
+    return icu
 with codecs.open('README.rst','r',encoding='utf-8') as readme_file:
     readme = readme_file.read()
 readme_file.close()
 requirements = [
+    icu1(),
     'nltk>=3.2.2',
     'future>=0.16.0',
     'six',
     'marisa_trie',
     'requests'
 ]
-class CustomInstallCommand(install):
-    """Customized setuptools install command - prints a friendly greeting."""
-    def run(self):
-        import pip
-        try:
-            import icu
-            print("OK")
-        except ImportError:
-            if windows_is()==True:
-                python_version='{0[0]}.{0[1]}'.format(sys.version_info)
-                bit=struct.calcsize("P") * 8
-                if python_version=="3.6":
-                    if bit=="64":
-                        pip.main(['install','https://github.com/wannaphongcom/pythainlp-installer/raw/master/windows/pyicu/PyICU-cp36-cp36m-win_amd64.whl'])
-                    else:
-                        pip.main(['install','https://github.com/wannaphongcom/pythainlp-installer/raw/master/windows/pyicu/PyICU-cp36-cp36m-win32.whl'])
-                elif python_version=="3.5":
-                    if bit=="64":
-                        pip.main(['install','https://github.com/wannaphongcom/pythainlp-installer/raw/master/windows/pyicu/PyICU-cp35-cp35m-win_amd64.whl'])
-                    else:
-                        pip.main(['install','https://github.com/wannaphongcom/pythainlp-installer/raw/master/windows/pyicu/PyICU-cp35-cp35m-win32.whl'])
-                elif python_version=="3.4":
-                    if bit=="64":
-                        pip.main(['install','https://github.com/wannaphongcom/pythainlp-installer/raw/master/windows/pyicu/PyICU-cp34-cp34m-win_amd64.whl'])
-                    else:
-                        pip.main(['install','https://github.com/wannaphongcom/pythainlp-installer/raw/master/windows/pyicu/PyICU-cp34-cp34m-win32.whl'])
-                elif python_version=="2.7":
-                    if bit=="64":
-                        pip.main(['install','https://github.com/wannaphongcom/pythainlp-installer/raw/master/windows/pyicu/PyICU-cp27-cp27m-win_amd64.whl'])
-                    else:
-                        pip.main(['install','https://github.com/wannaphongcom/pythainlp-installer/raw/master/windows/pyicu/PyICU-cp27-cp27m-win32.whl'])
-                else:
-                    pip.main(['install','pyicu'])
-            else:
-                pip.main(['install','pyicu'])
-        install.run(self)
 test_requirements = [
     # TODO: put package test requirements here
 ]
 
 setup(
-    cmdclass={'install': CustomInstallCommand},
     name='pythainlp',
     version='1.5.3',
     description="Thai natural language processing in Python package.",

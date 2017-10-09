5c12f9182478037a0bddfeac92cb62c16cb42fca
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2017-10-10 00:28:07 +0700

    add pyicu installer

--- pythainlp_windows_install.py
+++ pythainlp_windows_install.py
@@ -1,15 +1,11 @@
 ﻿# -*- coding: utf-8 -*-
 from setuptools import setup
 from setuptools import find_packages
-import codecs
-with codecs.open('README.rst','r',encoding='utf-8') as readme_file:
-    readme = readme_file.read()
-readme_file.close()
+import sys
 requirements = [
     'nltk>=3.2.2',
     'future>=0.16.0',
     'six',
-    'marisa_trie',
     'requests'
 ]
 
@@ -18,21 +14,19 @@ test_requirements = [
 ]
 
 setup(
-    name='pythainlp',
-    version='1.5.3',
-    description="Thai natural language processing in Python package.",
-    long_description=readme,
+    name='pythainlp_install_windows',
+    version='1.0',
+    description="Tool install pythainlp in windows.",
     author='Wannaphong Phatthiyaphaibun',
     author_email='wannaphong@yahoo.com',
-    url='https://sites.google.com/view/pythainlp/',
+    url='https://github.com/wannaphongcom/pythainlp',
     packages=find_packages(),
-    test_suite='pythainlp.test',
-    package_data={'pythainlp.corpus':['stopwords-th.txt','thaipos.json','thaiword.txt','corpus_license.md','tha-wn.db','new-thaidict.txt','negation.txt','provinces.csv'],'pythainlp.sentiment':['vocabulary.data','sentiment.data']},
     include_package_data=True,
     install_requires=requirements,
     license='Apache Software License 2.0',
     zip_safe=False,
     keywords='pythainlp',
+    scripts=[''],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -40,5 +34,4 @@ setup(
         'Natural Language :: Thai',
         'Topic :: Text Processing :: Linguistic',
         'Programming Language :: Python :: Implementation'],
-    script=['pyicu-installer.py']
 )

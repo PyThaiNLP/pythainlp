e6293da1b359677b85fbab0ceef771e2d74b9362
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2017-10-25 22:40:19 +0700

    fix Longest matching

--- pythainlp/tokenize/longest.py
+++ pythainlp/tokenize/longest.py
@@ -1,46 +1,120 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import,division,unicode_literals,print_function
-from builtins import *
-# Longest matching
-# โค้ดจาก https://stackoverflow.com/a/11642687
-from pythainlp.corpus.thaiword import get_data # ข้อมูลเก่า
-from math import log
+'''
+Code from https://github.com/patorn/thai-sentiment/blob/78bf461dfdc8a3f0517712fac56dd921dc0f9dd6/thai_sentiment/tokenizer.py
+'''
 import re
-def segment(s,data=get_data()):
-    """ตัดคำภาษาไทยด้วย Longest matching"""
-    words=data
-    wordcost = dict((k, log((i+1)*log(len(words)))) for i,k in enumerate(words))
-    maxword = max(len(x) for x in words)
+from pythainlp.tools import file_trie
+FRONT_DEP_CHAR = ['ะ', 'ั', 'า ', 'ำ', 'ิ', 'ี', 'ึ', 'ื', 'ุ', 'ู', 'ๅ', '็', '์', 'ํ']
+REAR_DEP_CHAR = ['ั', 'ื', 'เ', 'แ', 'โ', 'ใ', 'ไ', 'ํ']
+TONAL_CHAR = ['่', '้', '๊', '๋']
+ENDING_CHAR = ['ๆ', 'ฯ']
 
-    # Find the best match for the i first characters, assuming cost has
-    # been built for the i-1 first characters.
-    # Returns a pair (match_cost, match_length).
-    data = re.split(r'\n|\s+',s) # แยกช่องว่างและขึ้นประโยคใหม่
-    outall=''
-    def best_match(i):
-        candidates = enumerate(reversed(cost[max(0, i-maxword):i]))
-        return min((c + wordcost.get(s[i-k-1:i], 9e999), k+1) for k,c in candidates)
-    # Build the cost array.
-    countlist=0
-    while countlist<len(data):
-        s=data[countlist]
-        cost = [0]
-        for i in range(1,len(s)+1):
-            c,k = best_match(i)
-            cost.append(c)
-        # Backtrack to recover the minimal-cost string.
-        out = []
-        i = len(s)
-        while i>0:
-            c,k = best_match(i)
-            out.append(s[i-k:i])
-            i -= k
-        if countlist==0:
-            outall+='|'.join(list(reversed(out)))
+class Tokenizer(object):
+
+    def __init__(self,data=''):
+        if(data==''):
+            self._trie = file_trie(data="old")
         else:
-            outall+='|'+'|'.join(list(reversed(out)))
-        countlist+=1
-    return outall.split('|')
+            import marisa_trie
+            self._trie = marisa_trie.Trie(data)
+
+    @property
+    def trie(self):
+        return self._trie
+
+    def search_special_characters(self, characters):
+        match = re.search(u"[A-Za-z\d]*", characters)
+        if match.group(0):
+            return match.group(0).lower()
+        else:
+            return None
+
+    def is_next_word_valid(self, text, begin_position):
+        N = len(text)
+        characters = text[begin_position:N].strip()
+
+        if len(characters) == 0:
+            return True
+
+        match = self.search_special_characters(characters)
+        if match:
+            return True
+
+        for position in range(N):
+            if characters[0:position] in self.trie:
+                return True
+
+        return False
+
+    def longest_matching(self, text, begin_position):
+        N = len(text)
+        characters = text[begin_position:N]
+
+        match = self.search_special_characters(characters)
+        if match:
+            return match
+
+        word = None
+        word_valid = None
+
+        for position in range(N):
+            if characters[0:position] in self.trie:
+                word = characters[0:position]
+                if self.is_next_word_valid(characters, position):
+                    word_valid = characters[0:position]
+
+        if word:
+            if not word_valid:
+                word_valid = word
+
+            try:
+                if characters[len(word_valid)] in ENDING_CHAR:
+                    return characters[0:(len(word_valid) + 1)]
+                else:
+                    return word_valid
+            except:
+                return word_valid
+        else:
+            return ''
+
+    def segment_text(self, text):
+        begin_position = 0
+        N = len(text)
+        tokens = []
+        token_statuses = []
+        while(begin_position < N):
+            match = self.longest_matching(text, begin_position)
+            if not match:
+                if begin_position != 0 and \
+                    not text[begin_position].isspace() \
+                    and (text[begin_position] in FRONT_DEP_CHAR \
+                    or text[begin_position - 1] in REAR_DEP_CHAR \
+                    or text[begin_position] in TONAL_CHAR \
+                    or (token_statuses and token_statuses[-1] == 'unknown')):
+                    tokens[-1] += text[begin_position]
+                    token_statuses[-1] = 'unknown'
+                else:
+                    tokens.append(text[begin_position])
+                    token_statuses.append('unknown')
+                begin_position += 1
+            else:
+                if begin_position != 0 \
+                    and text[begin_position - 1] in REAR_DEP_CHAR:
+                    tokens[-1] += match
+                else:
+                    tokens.append(match)
+                    token_statuses.append('known')
+                begin_position += len(match)
+
+        return tokens
+
+    def tokenize(self, text):
+        tokens = self.segment_text(text)
+        return tokens
+def segment(s,data=''):
+    """ตัดคำภาษาไทยด้วย Longest matching"""
+    return Tokenizer(data).tokenize(s)
 if __name__ == "__main__":
 	s = 'สวัสดีชาวโลกเข้าใจกันไหมพวกคุณ โอเคกันไหม ยสยา ดีแล้วนะคุณเธอ'
 	print(segment(s))

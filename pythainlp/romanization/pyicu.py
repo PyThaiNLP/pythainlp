a2fbda12d422598c9d823032bc29baffb6b7129d
nyamakawa <nyamakawa2@gmail.com>
2017-12-25 13:50:41 +0900

    Fix "NameError: name 'sys' is not defined" when icu was not installed

--- pythainlp/romanization/pyicu.py
+++ pythainlp/romanization/pyicu.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import sys
 from __future__ import absolute_import,unicode_literals
 try:
 	import icu
--- pythainlp/tokenize/pyicu.py
+++ pythainlp/tokenize/pyicu.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import sys
 from __future__ import absolute_import,print_function,unicode_literals
 from six.moves import zip
 try:

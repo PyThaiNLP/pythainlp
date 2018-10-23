ec00aa7860be096ca427adc6755d9859445d1769
Arthit Suriyawongkul <arthit@gmail.com>
2018-10-23 11:43:59 +0700

    revert MetaSound module name for now

--- pythainlp/__init__.py
+++ pythainlp/__init__.py
@@ -3,7 +3,7 @@ from pythainlp.change import texttoeng, texttothai
 from pythainlp.collation import collation
 from pythainlp.date import now
 from pythainlp.keywords import find_keyword
-from pythainlp.metasound import metasound
+from pythainlp.Metasound import Metasound
 from pythainlp.rank import rank
 from pythainlp.romanization import romanize
 from pythainlp.sentiment import sentiment

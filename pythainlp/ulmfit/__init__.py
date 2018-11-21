9aa5a88cbc1a36f19305f229b3a9abbeb9add631
Ubuntu <ubuntu@ip-172-31-38-199.us-west-2.compute.internal>
2018-11-21 09:07:47 +0000

    add lm testers

--- pythainlp/ulmfit/__init__.py
+++ pythainlp/ulmfit/__init__.py
@@ -81,18 +81,58 @@ def get_path(fname):
 THWIKI = [get_path(MODEL_NAME)[:-4], get_path(ITOS_NAME)[:-4]]
 tt = ThaiTokenizer()
 
-def document_vector(ss, learn, data):
+def document_vector(text, learn, data):
     """
-    :meth: `document_vector` get document vector using pretrained ULMFiT model
-    :param str ss: sentence to extract embeddings
+    :meth: `document_vector` get document vector using fastai language model and data bunch
+    :param str text: sentence to extract embeddings
     :param learn: fastai language model learner
     :param data: fastai data bunch
-    :return: `numpy.array` of document vector sized 400
+    :return: `numpy.array` of document vector sized 1200 containing last hidden layer as well as its average pooling and max pooling
     """
-    s = tt.tokenizer(ss)
+    s = tt.tokenizer(text)
     t = torch.tensor(data.vocab.numericalize(s), requires_grad=False)[:,None].to(device)
     m = learn.model[0]
     m.reset()
     pred,_ = m(t)
-    res = pred[-1][-1,:,:].squeeze().detach().numpy()
+    #return concatenation of last, mean and max
+    last = pred[-1][-1,:,:].squeeze()
+    avg_pool = pred[-1].mean(0)[0].squeeze()
+    max_pool = pred[-1].max(0)[0].squeeze()
+    res = torch.cat((last,avg_pool,max_pool)).detach().cpu().numpy()
     return(res)
+
+def predict_word(text,learn,data,topk=5):
+    """
+    :meth: `predict_word` predicts top-k most likely words based on given string, fastai language model and data bunch
+    :param str text: seed text
+    :param learn: fastai language model learner
+    :param data: fastai data bunch
+    :param int topk: how many top-k words to generate
+    :return: list of top-k words
+    """    
+    s = tt.tokenizer(text)
+    t = torch.LongTensor(data.train_ds.vocab.numericalize(s)).view(-1,1).to(device)
+    t.requires_grad = False
+    m = learn.model
+    m.reset()
+    pred,*_ = m(t)
+    pred_i = pred[-1].topk(topk)[1].cpu().numpy()
+    return([data.vocab.itos[i] for i in pred_i])
+
+def predict_sentence(text,learn,data,nb_words=10):
+    result = []
+    s = tt.tokenizer(text)
+    t = torch.LongTensor(data.train_ds.vocab.numericalize(s)).view(-1,1).to(device)
+    t.requires_grad = False
+    m = learn.model
+    m.reset()
+    pred,*_ = m(t)
+    for i in range(nb_words):
+        pred_i = pred[-1].topk(2)[1]
+        #get first one if not unknowns, pads, or spaces
+        pred_i = pred_i[1] if pred_i.data[0] == 0 else pred_i[0]
+        pred_i = pred_i.view(-1,1)
+        result.append(data.train_ds.vocab.textify(pred_i))
+        t = torch.cat((t,pred_i))
+        pred,*_ = m(t)
+    return(result)

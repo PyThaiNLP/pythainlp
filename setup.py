83167467479d4d96afbf96cea49d728cd765efec
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2019-01-13 01:20:13 +0700

    del old file

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with codecs.open("requirements.txt", "r", encoding="utf-8") as f:
 setup(
     name="pythainlp",
     version="1.7.2",
-    description="Thai natural language processing library",
+    description="Thai Natural Language Processing in Python.",
     long_description=readme,
     long_description_content_type="text/markdown",
     author="PyThaiNLP",

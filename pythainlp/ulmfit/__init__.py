f79ecd6c4c0bf965d259937d4900cebe54214f10
root <cebril@gmail.com>
2019-02-24 15:10:04 +0000

    thai2fit 0.31; fastai>=1.0.38

--- pythainlp/ulmfit/__init__.py
+++ pythainlp/ulmfit/__init__.py
@@ -5,7 +5,6 @@ Code by https://github.com/cstorm125/thai2fit/
 """
 import collections
 import re
-from typing import Collection, List
 
 import numpy as np
 import torch
@@ -15,6 +14,7 @@ from fastai.text.transform import (
     fix_html,
     rm_useless_spaces,
     spec_add_spaces,
+    replace_all_caps,
 )
 from pythainlp.corpus import download, get_corpus_path
 from pythainlp.tokenize import word_tokenize
@@ -23,18 +23,13 @@ from pythainlp.util import normalize as normalize_char_order
 __all__ = [
     "ThaiTokenizer",
     "document_vector",
-    "predict_word",
-    "predict_sentence",
     "merge_wgts",
 ]
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-_MODEL_NAME_QRNN = "wiki_lm_qrnn"
-_ITOS_NAME_QRNN = "wiki_itos_qrnn"
 _MODEL_NAME_LSTM = "wiki_lm_lstm"
 _ITOS_NAME_LSTM = "wiki_itos_lstm"
-
 
 # Download pretrained models
 def _get_path(fname):
@@ -57,10 +52,10 @@ class ThaiTokenizer(BaseTokenizer):
     https://docs.fast.ai/text.transform#BaseTokenizer
     """
 
-    def __init__(self, lang: str = "th"):
+    def __init__(self, lang = "th"):
         self.lang = lang
 
-    def tokenizer(self, t: str) -> List[str]:
+    def tokenizer(self, t):
         """
         :meth: tokenize text with a frozen newmm engine
         :param str t: text to tokenize
@@ -68,27 +63,27 @@ class ThaiTokenizer(BaseTokenizer):
         """
         return word_tokenize(t, engine="ulmfit")
 
-    def add_special_cases(self, toks: Collection[str]):
+    def add_special_cases(self, toks):
         pass
 
 
-def replace_rep_after(t: str) -> str:
+def replace_rep_after(t):
     "Replace repetitions at the character level in `t` after the repetition"
 
-    def _replace_rep(m: Collection[str]) -> str:
+    def _replace_rep(m):
         c, cc = m.groups()
-        return f" {c} {TK_REP} {len(cc)+1} "
+        return f"{c} {TK_REP} {len(cc)+1} "
 
     re_rep = re.compile(r"(\S)(\1{3,})")
     return re_rep.sub(_replace_rep, t)
 
 
-def rm_useless_newlines(t: str) -> str:
+def rm_useless_newlines(t):
     "Remove multiple newlines in `t`."
     return re.sub(r"[\n]{2,}", " ", t)
 
 
-def rm_brackets(t: str) -> str:
+def rm_brackets(t):
     "Remove all empty brackets from `t`."
     new_line = re.sub(r"\(\)", "", t)
     new_line = re.sub(r"\{\}", "", new_line)
@@ -98,99 +93,33 @@ def rm_brackets(t: str) -> str:
 
 # Pretrained paths
 # TODO: Let the user decide if they like to download (at setup?)
-_THWIKI_QRNN = [_get_path(_MODEL_NAME_QRNN)[:-4], _get_path(_ITOS_NAME_QRNN)[:-4]]
 _THWIKI_LSTM = [_get_path(_MODEL_NAME_LSTM)[:-4], _get_path(_ITOS_NAME_LSTM)[:-4]]
 
 # Preprocessing rules for Thai text
-thai_rules = [
-    fix_html,
-    deal_caps,
-    replace_rep_after,
-    normalize_char_order,
-    spec_add_spaces,
-    rm_useless_spaces,
-    rm_useless_newlines,
-    rm_brackets,
-]
+pre_rules_th = [fix_html, replace_rep_after, normalize_char_order, 
+                spec_add_spaces, rm_useless_spaces, rm_useless_newlines, rm_brackets]
+post_rules_th = [replace_all_caps, deal_caps]
 
-_tokenizer = Tokenizer(tok_func=ThaiTokenizer, lang="th", pre_rules=thai_rules)
+_tokenizer = ThaiTokenizer()
 
 
-def document_vector(text: str, learn, data):
+def document_vector(text, learn, data):
     """
     :meth: `document_vector` get document vector using fastai language model and data bunch
     :param str text: text to extract embeddings
     :param learn: fastai language model learner
-    :param data: fastai data bunch
-    :return: `numpy.array` of document vector sized 1,200 containing last hidden layer as well as its average pooling and max pooling
+    :param data: fastai data bunch 
+    :return: `numpy.array` of document vector sized 400 based on the encoder of the model
     """
-    s = _tokenizer.tok_func.tokenizer(_tokenizer, text)
-    t = torch.tensor(data.vocab.numericalize(s), requires_grad=False)[:, None].to(
-        device
-    )
-    m = learn.model[0]
-    m.reset()
-    pred, _ = m(t)
-
-    # return concatenation of last, mean and max
-    last = pred[-1][-1, :, :].squeeze()
-    avg_pool = pred[-1].mean(0)[0].squeeze()
-    max_pool = pred[-1].max(0)[0].squeeze()
-    res = torch.cat((last, avg_pool, max_pool)).detach().cpu().numpy()
-
-    return res
+    
+    s = _tokenizer.tokenizer(text)
+    t = torch.tensor(data.vocab.numericalize(s), requires_grad=False).to(device)
+    m = learn.model[0].encoder.to(device)
+    res = m(t).mean(0).cpu().detach().numpy()
+    return(res)
 
 
-def predict_word(text: str, learn, data, topk: int = 5):
-    """
-    :meth: `predict_word` predicts top-k most likely words based on given string, fastai language model and data bunch
-    :param str text: seed text
-    :param learn: fastai language model learner
-    :param data: fastai data bunch
-    :param int topk: how many top-k words to generate
-    :return: list of top-k words
-    """
-    s = _tokenizer.tok_func.tokenizer(_tokenizer, text)
-    t = torch.LongTensor(data.train_ds.vocab.numericalize(s)).view(-1, 1).to(device)
-    t.requires_grad = False
-    m = learn.model
-    m.reset()
-    pred, *_ = m(t)
-    pred_i = pred[-1].topk(topk)[1].cpu().numpy()
-
-    return [data.vocab.itos[i] for i in pred_i]
-
-
-def predict_sentence(text: str, learn, data, nb_words: int = 10):
-    """
-    :meth: `predict_word` predicts subsequent sentences based on given string, fastai language model and data bunch
-    :param str text: seed text
-    :param learn: fastai language model learner
-    :param data: fastai data bunch
-    :param int nb_words: how many words of sentence to generate
-    :return: string of `nb_words` words
-    """
-    result = []
-    s = _tokenizer.tok_func.tokenizer(_tokenizer, text)
-    t = torch.LongTensor(data.train_ds.vocab.numericalize(s)).view(-1, 1).to(device)
-    t.requires_grad = False
-    m = learn.model
-    m.reset()
-    pred, *_ = m(t)
-
-    for _ in range(nb_words):
-        pred_i = pred[-1].topk(2)[1]
-        # get first one if not unknowns, pads, or spaces
-        pred_i = pred_i[1] if pred_i.data[0] == 0 else pred_i[0]
-        pred_i = pred_i.view(-1, 1)
-        result.append(data.train_ds.vocab.textify(pred_i))
-        t = torch.cat((t, pred_i))
-        pred, *_ = m(t)
-
-    return result
-
-
-def merge_wgts(em_sz: int, wgts, itos_pre, itos_new):
+def merge_wgts(em_sz, wgts, itos_pre, itos_new):
     """
     :meth: `merge_wgts` insert pretrained weights and vocab into a new set of weights and vocab;
     use average if vocab not in pretrained vocab

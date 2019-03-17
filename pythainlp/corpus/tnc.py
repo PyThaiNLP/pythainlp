1b92f22db0e2b911497dbf28506c5201ce37f3af
Wannaphong <wannaphong@kkumail.com>
2019-03-18 01:03:49 +0700

    fix path tnc

--- pythainlp/corpus/tnc.py
+++ pythainlp/corpus/tnc.py
@@ -54,7 +54,7 @@ def word_freqs():
     ดึงข้อมูลความถี่คำของ Thai National Corpus มาใช้งาน
     โดยจะได้ข้อมูลในรูปแบบ List[Tuple] [(word,frequency),...]
     """
-    with open(get_corpus("tnc_freq.txt"), "r", encoding="utf-8") as f:
+    with open(str(get_corpus("tnc_freq.txt")), "r", encoding="utf-8") as f:
         lines = f.read().splitlines()
     f.close()
 

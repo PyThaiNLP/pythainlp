af3e59f100e8d8444fabac6d2555711071f03a4b
Arthit Suriyawongkul <arthit@gmail.com>
2019-04-12 18:40:39 +0200

    TTC will now use local copy of tcc_freq.txt

--- pythainlp/corpus/ttc.py
+++ pythainlp/corpus/ttc.py
@@ -5,29 +5,22 @@ Thai Textbook Corpus (TTC) word frequency
 Credit: Korakot Chaovavanich‎
 https://www.facebook.com/photo.php?fbid=363640477387469&set=gm.434330506948445&type=3&permPage=1
 """
-import os
 
-from pythainlp.corpus import download as download_data
-from pythainlp.tools import get_full_data_path
+from pythainlp.corpus import get_corpus
 
 __all__ = ["word_freqs"]
+
+_FILENAME = "ttc_freq.txt"
 
 
 def word_freqs():
     """
     Get word frequency from Thai Textbook Corpus (TTC)
     """
-    path = get_full_data_path("ttc_freq.txt")  # try local copy first
-    if not os.path.exists(path):  # if fail, download from internet
-        download_data("ttc")
-
-    with open(path, "r", encoding="utf8") as f:
-        lines = f.read().splitlines()
-    f.close()
-
+    lines = list(get_corpus(_FILENAME))
     listword = []
     for line in lines:
-        listindata = line.split("	")
+        listindata = line.split("\t")
         listword.append((listindata[0], int(listindata[1])))
 
     return listword

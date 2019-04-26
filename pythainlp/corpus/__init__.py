bc5c6c92615024f8167c568a3b79ea60e7556741
Wannaphong <wannaphong@kkumail.com>
2019-04-27 01:22:25 +0700

    clean pep8 for thai2rom

--- pythainlp/corpus/__init__.py
+++ pythainlp/corpus/__init__.py
@@ -14,8 +14,8 @@ _CORPUS_DIRNAME = "corpus"
 _CORPUS_PATH = os.path.join(get_pythainlp_path(), _CORPUS_DIRNAME)
 
 _CORPUS_DB_URL = (
-    "https://raw.githubusercontent.com/"+
-    "PyThaiNLP/pythainlp-corpus/"+
+    "https://raw.githubusercontent.com/" +
+    "PyThaiNLP/pythainlp-corpus/" +
     "master/db.json"
 )
 

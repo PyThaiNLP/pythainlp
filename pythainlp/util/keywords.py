a4e94624ed19f0ac6dfdfa60ef19729708b1ae41
Chakri Lowphansirikul <arta@Chakris-MacBook-Pro-2.local>
2019-06-10 12:40:20 +0700

    todo_word_util_4: provide example for rank

--- pythainlp/util/keywords.py
+++ pythainlp/util/keywords.py
@@ -9,11 +9,45 @@ _STOPWORDS = thai_stopwords()
 
 def rank(words: List[str], exclude_stopwords: bool = False) -> Counter:
     """
-    Sort words by frequency
+    Count word frequecy given a list of Thai words with an option to exclude stopwords.
 
     :param list words: a list of words
-    :param bool exclude_stopwords: exclude stopwords
-    :return: Counter
+    :param bool exclude_stopwords: If this parameter is set to **True** to exclude stopwords from counting. Otherwise, the stopwords will be counted. By default, `exclude_stopwords` is set to **False**
+    :return: a Counter object representing word frequency from the text
+    :rtype: :class:`collections.Counter`
+    
+    :Example:
+
+        Include stopwords in counting word frequency:
+
+        >>> from pythainlp.util import rank
+        >>> 
+        >>> words = ["บันทึก", "เหตุการณ์", " ", "มี", "การ", "บันทึก", "เป็น", " ", "ลายลักษณ์อักษร"]
+        >>> rank(words)
+        Counter(
+            {
+                ' ': 2,
+                'การ': 1,
+                'บันทึก': 2,
+                'มี': 1,
+                'ลายลักษณ์อักษร': 1,
+                'เป็น': 1,
+                'เหตุการณ์': 1
+            })
+
+        Exclude stopword in counting word frequency:
+
+        >>> from pythainlp.util import rank
+        >>> 
+        >>> words = ["บันทึก", "เหตุการณ์", " ", "มี", "การ", "บันทึก", "เป็น", " ", "ลายลักษณ์อักษร"]
+        >>> rank(words)
+        Counter(
+            {
+                ' ': 2, 
+                'บันทึก': 2, 
+                'ลายลักษณ์อักษร': 1, 
+                'เหตุการณ์': 1
+            })
     """
     if not words:
         return None

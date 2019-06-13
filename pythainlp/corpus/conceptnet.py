6f3efe2054517d35b197a0a4dd1a6406e8c86a98
Chakri Lowphansirikul <arta@Chakris-MacBook-Pro-2.local>
2019-06-13 17:43:42 +0700

    todo_corpus_25: briefly explain conceptnet.edges

--- pythainlp/corpus/conceptnet.py
+++ pythainlp/corpus/conceptnet.py
@@ -7,10 +7,24 @@ import requests
 
 def edges(word: str, lang: str = "th"):
     """
-    Get edges from ConceptNet API
+    Get edges from `ConceptNet <http://api.conceptnet.io/>`_ API.
+    ConceptNet is a public semantic network, designed to help computers understand the meanings of words that people use.
 
-    :param str word: word
-    :param str lang: language
+    For example, the term "ConceptNet" is a "knowledge graph", and "knowledge graph" has "common sense knowledge" which is a  part of "artificial inteligence".
+    Also, "ConcepNet" is used for "natural language understanding" which is a part of "artificial intelligence". 
+ 
+        | "ConceptNet" --is a--> "knowledge graph" --has--> "common sense" --a part of--> "artificial intelligence" 
+        | "ConceptNet" --used for--> "natural language understanding" --a part of--> "artificial intelligence" 
+    
+    With this illustration, it shows relationships (represented as *Edge*) between the terms (represented as *Node*)
+
+   
+
+    :param str word: word to be sent to ConceptNet API
+    :param str lang: abbreviation of language (i.e. *th* for Thai, *en* for English, or *ja* for Japan). By default, it is *th* (Thai).
+
+    :return: return edges of the given word according to the ConceptNet network. 
+    :rtype: list[dict]
     """
 
     obj = requests.get(f"http://api.conceptnet.io/c/{lang}/{word}").json()

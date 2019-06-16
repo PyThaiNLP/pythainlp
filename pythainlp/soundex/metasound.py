c15aa647b6912a2b9dd1c385009214965f0888d5
Chakri Lowphansirikul <arta@Chakris-MacBook-Pro-2.local>
2019-06-16 21:29:00 +0700

    fix pep8 issue

--- pythainlp/soundex/metasound.py
+++ pythainlp/soundex/metasound.py
@@ -3,8 +3,9 @@
 Thai soundex - MetaSound system
 
 References:
-Snae & Brückner. (2009). Novel Phonetic Name Matching Algorithm with a Statistical
-Ontology for Analysing Names Given in Accordance with Thai Astrology.
+Snae & Brückner. (2009). Novel Phonetic Name Matching Algorithm with
+a Statistical Ontology for Analysing Names Given in Accordance
+with Thai Astrology.
 https://pdfs.semanticscholar.org/3983/963e87ddc6dfdbb291099aa3927a0e3e4ea6.pdf
 """
 

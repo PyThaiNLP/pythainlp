24655938411ceb7d7f15479e3f7da9d2a89152b4
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2019-08-03 18:08:15 +0700

    Update test_ulmfit.py

--- tests/test_ulmfit.py
+++ tests/test_ulmfit.py
@@ -5,8 +5,14 @@ import os
 import sys
 import unittest
 
+from pythainlp.ulmfit import *
 
 class TestUlmfitPackage(unittest.TestCase):
 
-    def setUp():
-        pass
+    def test_ThaiTokenizer(self):
+        self.thai = ThaiTokenizer()
+        self.assertIsNotNone(self.thai.tokenizer("ทดสอบการตัดคำ"))
+    def test_rm_useless_newlines(self):
+        self.assertIsNotNone(rm_useless_newlines("ผมจะขึ้นบรรทัดใหม่\nคุณโอเคไหม"))
+    def test_lowercase_all(self):
+        self.assertIsNotNone(lowercase_all(["A","B"]))

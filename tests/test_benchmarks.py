7774c2e5fe7eaa35a474e87e3fc615ca54cbfc37
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2019-08-10 02:10:53 +0700

    Add test benchmarks

---
+++ tests/test_benchmarks.py
@@ -0,0 +1,13 @@
+import datetime
+import os
+import sys
+import unittest
+
+from pythainlp.benchmarks import *
+
+class TestBenchmarksPackage(unittest.TestCase):
+
+    def test_preprocessing(self):
+        self.assertIsNotNone(preprocessing(sample="ทดสอบ การ ทำ ความสะอาด ข้อมูล<tag>ok</tag>"))
+    def test_benchmark(self):
+        self.assertIsNotNone(benchmark(["วัน","จัน","ทร์","สี","เหลือง"],["วัน","จันทร์","สี","เหลือง"]))

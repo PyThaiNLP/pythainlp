6fb9fe335fbcd19e1329ccaeab742d6e3b5b09de
heytitle <pat.chormai@gmail.com>
2019-08-18 12:20:46 +0200

    add syllable tokenization command

--- pythainlp/cli/tokenization.py
+++ pythainlp/cli/tokenization.py
@@ -1,8 +1,14 @@
 import argparse
 
-from pythainlp.tokenize import word_tokenize
+from pythainlp.tokenize import word_tokenize, syllable_tokenize
 
 class App:
+
+    default_engine = {
+        "word": "newmm",
+        "syllable": "ssg"
+    }
+
     def __init__(self, argv):
         parser = argparse.ArgumentParser("corpus")
         parser.add_argument("subcommand",
@@ -11,6 +17,11 @@ class App:
         )
 
         parser.add_argument("--text",
+            type=str,
+            help="text",
+        )
+
+        parser.add_argument("--engine",
             type=str,
             help="text",
         )
@@ -24,10 +35,22 @@ class App:
             print("No subcommand available: %s" % subcommand)
 
     @staticmethod
-    def word(args):
-        tokens = word_tokenize(args.text)
+    def word(args, default_engine="newmm"):
+        engine = App._get_engine(args)
+
+        tokens = word_tokenize(args.text, engine=engine)
         print("|".join(tokens))
 
     @staticmethod
-    def syllable(args):
-        print("kenize syllable")
+    def syllable(args, default_engine="ssg"):
+        engine = App._get_engine(args)
+
+        syllables = syllable_tokenize(args.text)
+        print("|".join(syllables))
+
+    @staticmethod
+    def _get_engine(args):
+        engine = args.engine if args.engine else App.default_engine[args.subcommand]
+        print(f"Using engine={engine}")
+
+        return engine

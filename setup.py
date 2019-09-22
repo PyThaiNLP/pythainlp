00d68ea2af7ff7b37d69b1a3ab572fc13c82cd39
bact <arthit@gmail.com>
2019-09-22 17:00:32 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ readme = """
 ![PyThaiNLP Logo](https://avatars0.githubusercontent.com/u/32934255?s=200&v=4)
 
 PyThaiNLP is a Python library for Thai natural language processing.
-It includes word tokenizers, transliterators, soundex converters, part-of-speech taggers, and spell checkers.
+The library provides functions like word tokenization, part-of-speech tagging,
+transliteration, soundex generation, and spell checking.
 
 ## Install
 
@@ -16,9 +17,11 @@ For stable version:
 pip install pythainlp
 ```
 
-Some functionalities, like word vector, required extra packages. See https://github.com/PyThaiNLP/pythainlp for installation options.
+Some functionalities, like word vector, required extra packages.
+See https://github.com/PyThaiNLP/pythainlp for installation options.
 
-**Note for Windows**: `marisa-trie` wheels can be obtained from https://www.lfd.uci.edu/~gohlke/pythonlibs/#marisa-trie
+**Note for Windows**: `marisa-trie` wheels can be obtained from
+https://www.lfd.uci.edu/~gohlke/pythonlibs/#marisa-trie
 Install it with pip, for example: `pip install marisa_trie‑xxx.whl`
 """
 
@@ -32,32 +35,32 @@ requirements = [
 ]
 
 extras = {
-    "artagger": ["artagger"],
-    "attacut": ["attacut"],
-    "benchmarks": ["numpy", "pandas"],
-    "deepcut": ["deepcut", "keras", "tensorflow"],
-    "icu": ["pyicu"],
-    "ipa": ["epitran"],
-    "ml": ["keras", "numpy", "torch"],
-    "ner": ["sklearn-crfsuite"],
-    "ssg": ["ssg"],
-    "thai2fit": ["emoji", "gensim", "numpy"],
-    "thai2rom": ["torch", "numpy"],
+    "artagger": ["artagger>=0.1.0.3"],
+    "attacut": ["attacut>=1.0.2"],
+    "benchmarks": ["numpy>=1.17.2", "pandas>=0.25.1"],
+    "deepcut": ["deepcut>=0.6.1.0", "keras>=2.3.0", "tensorflow>=1.14.0"],
+    "icu": ["pyicu>=2.3.1"],
+    "ipa": ["epitran>=1.1"],
+    "ml": ["keras>=2.3.0", "numpy>=1.17.2", "torch>=1.2.0"],
+    "ner": ["sklearn-crfsuite>=0.3.6"],
+    "ssg": ["ssg>=0.0.4"],
+    "thai2fit": ["emoji>0.5.4", "gensim>=3.8.0", "numpy>=1.17.2"],
+    "thai2rom": ["torch>=1.2.0", "numpy>=1.17.2"],
     "full": [
-        "artagger",
-        "attacut",
-        "deepcut",
-        "epitran",
-        "gensim",
-        "keras",
-        "numpy",
-        "pyicu",
-        "sklearn-crfsuite",
-        "tensorflow",
-        "torch",
-        "ssg",
-        "emoji",
-        "pandas",
+        "artagger>=0.1.0.3",
+        "attacut>=1.0.2",
+        "deepcut>=0.6.1.0",
+        "epitran>=1.1",
+        "gensim>=3.8.0",
+        "keras>=2.3.0",
+        "numpy>=1.17.2",
+        "pyicu>=2.3.1",
+        "sklearn-crfsuite>=0.3.6",
+        "tensorflow>=1.14.0",
+        "torch>=1.2.0",
+        "ssg>=0.0.4",
+        "emoji>=0.5.4",
+        "pandas>=0.25.1",
     ],
 }
 

b3e581d5abf47372a31ab489f0b8937f936d5f65
Chakri Lowphansirikul <artificialcc@gmail.com>
2019-09-25 16:53:46 +0700

    Fix pep8 issues

--- pythainlp/tokenize/trie.py
+++ pythainlp/tokenize/trie.py
@@ -1,30 +1,32 @@
 # -*- coding: utf-8 -*-
-
 class Trie:
+    
     class Node(object):
         __slots__ = 'end', 'children'
         def __init__(self):
-            self.end = False 
+            self.end = False
             self.children = {}
-      
+
     def __init__(self, words):
         self.words = words
         self.root = Trie.Node()
         for word in words:
             cur = self.root
-            for ch in word: 
+            for ch in word:
                 node = cur.children.get(ch)
-                if not node: 
-                    node = Trie.Node() 
-                    cur.children[ch] = node 
+                if not node:
+                    node = Trie.Node()
+                    cur.children[ch] = node
                 cur = node
-            cur.end = True 
+            cur.end = True
+
     def prefixes(self, text):
         res = []
         cur = self.root
         for i, ch in enumerate(text):
             node = cur.children.get(ch)
-            if not node: break
+            if not node:
+                break
             if node.end:
                 res.append(text[:i+1])
             cur = node

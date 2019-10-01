28f7256178f4b815ac2aefd9709efd08ff9ee30e
bact <arthit@gmail.com>
2019-10-02 02:06:59 +0700

    Remove tensorflow, keras, deepcut dependencies

--- setup.py
+++ setup.py
@@ -30,17 +30,16 @@ requirements = [
     "nltk>=3.4.5,<4",
     "requests>=2.22.0,<3",
     "tinydb>=3.13.0,<4",
-    "tqdm>=4.35.0,<5",
+    "tqdm>=4.32.2,<5",
 ]
 
 extras = {
     "artagger": ["artagger>=0.1.0.3"],
     "attacut": ["attacut>=1.0.2"],
     "benchmarks": ["numpy>=1.17.2", "pandas>=0.25.1"],
-    "deepcut": ["deepcut>=0.6.1.0", "keras>=2.3.0", "tensorflow>=1.14.0,<2"],
     "icu": ["pyicu>=2.3.1"],
     "ipa": ["epitran>=1.1"],
-    "ml": ["keras>=2.3.0", "numpy>=1.17.2", "torch>=1.2.0"],
+    "ml": ["numpy>=1.17.2", "torch>=1.2.0"],
     "ner": ["sklearn-crfsuite>=0.3.6"],
     "ssg": ["ssg>=0.0.4"],
     "thai2fit": ["emoji>0.5.4", "gensim>=3.8.0", "numpy>=1.17.2"],
@@ -48,18 +47,15 @@ extras = {
     "full": [
         "artagger>=0.1.0.3",
         "attacut>=1.0.2",
-        "deepcut>=0.6.1.0",
+        "emoji>=0.5.4",
         "epitran>=1.1",
         "gensim>=3.8.0",
-        "keras>=2.3.0",
         "numpy>=1.17.2",
+        "pandas>=0.25.1",
         "pyicu>=2.3.1",
         "sklearn-crfsuite>=0.3.6",
-        "tensorflow>=1.14.0,<2",
-        "torch>=1.2.0",
         "ssg>=0.0.4",
-        "emoji>=0.5.4",
-        "pandas>=0.25.1",
+        "torch>=1.2.0",
     ],
 }
 

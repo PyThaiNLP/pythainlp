ec2a5a151f55081fba8b0cc843a89905525c37e3
Peradon <peradon_charoenchainetr@hotmail.com>
2019-10-03 14:05:11 +0700

    update pn.py in spell module
    check for numeric type before do the operation when call correct function

--- pythainlp/spell/pn.py
+++ pythainlp/spell/pn.py
@@ -311,7 +311,20 @@ class NorvigSpellChecker:
         if not word:
             return ""
 
+        # Check for numeric type
+        try:
+            if "." in word:
+                float(word)
+            else:
+                int(word)
+            return word
+        except ValueError:
+            pass
+            
+
         return self.spell(word)[0]
+
+    
 
 
 DEFAULT_SPELL_CHECKER = NorvigSpellChecker()

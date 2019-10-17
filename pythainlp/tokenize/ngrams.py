6f436640e6fa8250edfff78df128d46161c50fc0
Subarna Lamsal <subarna.lamsal2018@vitstudent.ac.in>
2019-10-17 15:23:03 +0530

    Added n_grams for n items tokenization
    Ngram is the contiguous sequence of n items from a given sample of text or speech. Ngram model is the probabilistic language method for predicting the next item in the sequence in the form of (n-1) Markov Model.

---
+++ pythainlp/tokenize/ngrams.py
@@ -0,0 +1,15 @@
+#text='hello everyone i am subarna lamsal from nepal'
+#num=int(input("Enter the number of n_grams required"))
+
+
+def n_grams(text, ngram_num, i=0):
+    while (len(text[i:i+ngram_num]) == ngram_num):
+        print(text[i:i+ngram_num])
+        i = i+1
+
+# n_grams(text.split(),num)
+
+# PARAMETERS
+# text---> the given text
+# ngram_num ---> the no.of n_grams required
+# for example: 1 for unigram, 2 for bigram, 3 for trigram and so on..

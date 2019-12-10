d4de927bebbd28167ec5d4adf80722b4d8753baf
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2019-12-10 13:28:02 +0700

    Update attacut.py (build and deploy docs)

--- pythainlp/tokenize/attacut.py
+++ pythainlp/tokenize/attacut.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*
 """
 Wrapper for AttaCut - Fast and Reasonably Accurate Word Tokenizer for Thai
 :See Also:
@@ -9,6 +10,11 @@ from attacut import tokenize
 
 
 def segment(text: str) -> List[str]:
+    """
+    Wrapper for AttaCut - Fast and Reasonably Accurate Word Tokenizer for Thai
+    :param str text: text to be tokenized to words
+    :return: list of words, tokenized from the text
+    """
     if not text or not isinstance(text, str):
         return []
 

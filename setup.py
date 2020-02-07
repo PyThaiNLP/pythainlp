d424c9f4a5e6e3d6c8de768ae44c5d1e26e51757
bact <arthit@gmail.com>
2020-02-07 21:51:38 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ extras = {
 
 setup(
     name="pythainlp",
-    version="2.2dev0",
+    version="2.2.0-dev0",
     description="Thai Natural Language Processing library",
     long_description=readme,
     long_description_content_type="text/markdown",

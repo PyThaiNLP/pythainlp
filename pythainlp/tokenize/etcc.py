beff7bb4c9c3962869c74e5adcfa2fe8dfa3c1a1
bact <arthit@gmail.com>
2020-02-11 18:05:35 +0000

    Refactor etcc.py
    - Directly pass the iterable (list of words) to the Tokenizer
    - Pre-compile pattern "[ะาๆฯๅำ]"

--- pythainlp/tokenize/etcc.py
+++ pythainlp/tokenize/etcc.py
@@ -11,34 +11,48 @@ Inrut, Jeeragone, Patiroop Yuanghirun, Sarayut Paludkong, Supot Nitsuwat, and Pa
 "Thai word segmentation using combination of forward and backward longest matching techniques."
 In International Symposium on Communications and Information Technology (ISCIT), pp. 37-40. 2001.
 """
-from typing import List
 import re
+from typing import List
 
 from pythainlp.corpus import get_corpus
-from pythainlp.tokenize import Tokenizer,Trie
+from pythainlp.tokenize import Tokenizer
 
-_etcc_trie = Trie(list(get_corpus("etcc.dict")))
-_cut_etcc = Tokenizer(_etcc_trie, engine='longest')
+_cut_etcc = Tokenizer(get_corpus("etcc.dict"), engine="longest")
+_PAT_ENDING_CHAR = "[ะาๆฯๅำ]"
+_RE_ENDING_CHAR = re.compile(_PAT_ENDING_CHAR)
 
-def _cut_subword(listword: list) -> List[str]:
-  _j = len(listword)
-  _i = 0
-  while True:
-    if _i == _j:
-      break
-    if (re.search("[ะาๆฯๅำ]", listword[_i]) and _i > 0 and len(listword[_i]) == 1):
-      listword[_i - 1] += listword[_i]
-      del listword[_i]
-      _j -= 1
-    _i += 1
-  return listword
+
+def _cut_subword(tokens: list) -> List[str]:
+    _j = len(tokens)
+    _i = 0
+    while True:
+        if _i == _j:
+            break
+        if (
+            _RE_ENDING_CHAR.search(tokens[_i])
+            and _i > 0
+            and len(tokens[_i]) == 1
+        ):
+            tokens[_i - 1] += tokens[_i]
+            del tokens[_i]
+            _j -= 1
+        _i += 1
+    return tokens
+
 
 def segment(text: str) -> List[str]:
     """
-    Enhanced Thai Character Cluster (ETCC)
+    Segmenting text into ETCCs.
+
+    Enhanced Thai Character Cluster (ETCC) is a kind of subword unit.
+    The concept was presented in Inrut, Jeeragone, Patiroop Yuanghirun,
+    Sarayut Paludkong, Supot Nitsuwat, and Para Limmaneepraserth.
+    "Thai word segmentation using combination of forward and backward
+    longest matching techniques." In International Symposium on Communications
+    and Information Technology (ISCIT), pp. 37-40. 2001.
 
     :param str text: text to be tokenized to character clusters
-    :return: list of subwords (Enhanced Thai Character Cluster), tokenized from the text
+    :return: list of clusters, tokenized from the text
     :return: list[str]
     """
 

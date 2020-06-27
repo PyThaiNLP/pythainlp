330699b8764e6d99b41682d2d78a5e45b2f863ee
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-06-27 19:34:43 +0700

    Fixed test

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -6,8 +6,8 @@ from pythainlp.translate import translate
 
 class TestTranslatePackage(unittest.TestCase):
     def test_translate(self):
-        self.assertIsNone(translate("แมวกินปลา", source="th", target="en"))
-        self.assertIsNone(translate("แมวกินปลา", source="th", target="en", tokenizer="word"))
-        self.assertIsNone(translate("แมวกินปลา", source="th", target="en", tokenizer="bpe"))
-        self.assertIsNone(translate("the cat eats fish.", source="en", target="th"))
-        self.assertIsNone(translate("the cat eats fish.", source="en", target="th", tokenizer="bpe"))
+        self.assertIsNotNone(translate("แมวกินปลา", source="th", target="en"))
+        self.assertIsNotNone(translate("แมวกินปลา", source="th", target="en", tokenizer="word"))
+        self.assertIsNotNone(translate("แมวกินปลา", source="th", target="en", tokenizer="bpe"))
+        self.assertIsNotNone(translate("the cat eats fish.", source="en", target="th"))
+        self.assertIsNotNone(translate("the cat eats fish.", source="en", target="th", tokenizer="bpe"))

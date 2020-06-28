ead62c8a7c9ba37c16abb6ed234d18ffb5d409d7
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-06-28 13:54:18 +0700

    Update best.py

--- pythainlp/corpus/best.py
+++ pythainlp/corpus/best.py
@@ -9,8 +9,8 @@ cleanr = re.compile(r'<[^>]+>')
 
 
 def cleantag(text):
-  cleantext = re.sub(cleanr, '', text)
-  return cleantext
+    cleantext = re.sub(cleanr, '', text)
+    return cleantext
 
 
 def get_conll(text):
@@ -19,17 +19,17 @@ def get_conll(text):
     for i in list_data:
         word = cleantag(i)
         tag = "O"
-        find_tag = re.findall(cleanr,i)
+        find_tag = re.findall(cleanr, i)
         if find_tag != []:
-            tag = "B-"+find_tag[0].replace('<','').replace('>','')
-        temp.append((word,tag))
+            tag = "B-"+find_tag[0].replace('<', '').replace('>', '')
+        temp.append((word, tag))
     return temp
 
 
 class BEST_I:
     def __init__(self, path):
         self.path = path
-        self.list_file = list(glob.glob(os.path.join(self.path,"*.txt")))
+        self.list_file = list(glob.glob(os.path.join(self.path, "*.txt")))
         self.data = {}
         for i in self.list_file:
             filename = os.path.basename(i)
@@ -43,17 +43,19 @@ class BEST_I:
                     if j.count('|') > 1:
                         self.temp.append(j.split('|'))
                     else:
-                        self.temp.append(j.replace('|',''))
+                        self.temp.append(j.replace('|', ''))
 
                 self.data[filename]["words"] = self.temp
                 self.data[filename]["conll"] = [get_conll(j) for j in self.data[filename]["raw"]]
+
     def get_words_all(self):
         self.words_all = []
         for i in list(self.data.keys()):
             for j in self.data[i]["words"]:
                 self.words_all.append(j)
+
         return self.words_all
-    
+
     def count_word_all(self):
         self.words_all = self.get_words_all()
         self.list_word = []

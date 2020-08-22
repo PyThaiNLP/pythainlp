9bd9c0638690d3cfbdc8c1c181863c343781f764
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-08-22 17:28:07 +0700

    Update __init__.py

--- pythainlp/translate/__init__.py
+++ pythainlp/translate/__init__.py
@@ -5,6 +5,7 @@ Language translation.
 
 __all__ = [
     "translate",
+    "download_model_all"
 ]
 
-from pythainlp.translate.core import translate
+from pythainlp.translate.core import translate, download_model_all

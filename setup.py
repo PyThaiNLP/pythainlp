4d00fc4574a1d26a5fcd2ea767920387ba5332b5
Arthit Suriyawongkul <arthit@gmail.com>
2020-09-17 09:12:29 +0100

    Bump version: 2.2.4-beta0 → 2.2.4

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ extras = {
 
 setup(
     name="pythainlp",
-    version="2.2.4-beta0",
+    version="2.2.4",
     description="Thai Natural Language Processing library",
     long_description=readme,
     long_description_content_type="text/markdown",

b8ab03e67b66beb274924adde2af0f6420370f86
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-10-03 16:47:24 +0700

    crfcls: change features name

--- pythainlp/tokenize/crfcls.py
+++ pythainlp/tokenize/crfcls.py
@@ -14,20 +14,20 @@ def _doc2features(doc, i):
     curr_word = doc[i][0]
     curr_pos = doc[i][1]
     features = {
-        "word.word": curr_word,
-        "word.isspace": curr_word.isspace(),
-        "word.isdigit()": curr_word.isdigit(),
-        "postag": curr_pos,
+        "word.curr_word": curr_word,
+        "word.curr_isspace": curr_word.isspace(),
+        "word.curr_isdigit": curr_word.isdigit(),
+        "word.curr_postag": curr_pos,
     }
 
     # features from previous word
     if i > 0:
         prev_word = doc[i - 1][0]
         prev_pos = doc[i - 1][1]
-        features["word.prevword"] = prev_word
-        features["word.previsspace"] = prev_word.isspace()
-        features["word.prevwordisdigit"] = prev_word.isdigit()
-        features["word.prepostag"] = prev_pos
+        features["word.prev_word"] = prev_word
+        features["word.prev_isspace"] = prev_word.isspace()
+        features["word.prev_isdigit"] = prev_word.isdigit()
+        features["word.prev_postag"] = prev_pos
     else:
         features["BOS"] = True  # Beginning of Sequence
 
@@ -35,10 +35,10 @@ def _doc2features(doc, i):
     if i < len(doc) - 1:
         next_word = doc[i + 1][0]
         next_pos = doc[i + 1][1]
-        features["word.nextword"] = next_word
-        features["word.nextisspace"] = next_word.isspace()
-        features["word.nextwordisdigit"] = next_word.isdigit()
-        features["word.nextpostag"] = next_pos
+        features["word.next_word"] = next_word
+        features["word.next_isspace"] = next_word.isspace()
+        features["word.next_isdigit"] = next_word.isdigit()
+        features["word.next_postag"] = next_pos
     else:
         features["EOS"] = True  # End of Sequence
 

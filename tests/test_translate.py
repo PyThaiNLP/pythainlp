16009f5f2bd498987c861c3794ffdc38e8e83a8e
Arthit Suriyawongkul <arthit@gmail.com>
2020-10-17 20:30:18 +0100

    Remove duplicated test cases

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -7,26 +7,11 @@ from pythainlp.translate import translate
 
 class TestTranslatePackage(unittest.TestCase):
     def test_translate(self):
-        self.assertIsNotNone(translate("แมวกินปลา", source="th", target="en"))
         self.assertIsNotNone(
             translate(
                 "แมวกินปลา",
                 source="th",
                 target="en"
-            )
-        )
-        self.assertIsNotNone(
-            translate(
-                "แมวกินปลา",
-                source="th",
-                target="en"
-            )
-        )
-        self.assertIsNotNone(
-            translate(
-                "the cat eats fish.",
-                source="en",
-                target="th"
             )
         )
         self.assertIsNotNone(

4df4dac9be1e240471b5ea0da6ac84d89d0e44ac
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-12-29 13:11:35 +0700

    Add Translate class

--- pythainlp/translate/core.py
+++ pythainlp/translate/core.py
@@ -125,3 +125,70 @@ def translate(text: str, source: str, target: str) -> str:
         return ValueError("The combination of the arguments isn't allowed.")
 
     return translated
+
+class Translate:
+    def __init__(self):
+        self.target = None
+        self._model = None
+        self._model_name = None
+    def _scb_en_th_model_init(self):
+        if self._model_name != "scb_1m_en-th_moses":
+            self._model_name = "scb_1m_en-th_moses"
+            _download_install(self._model_name)
+            self._model = TransformerModel.from_pretrained(
+                model_name_or_path=_get_translate_path(
+                    self._model_name, _TH_EN_FILE_NAME, "models",
+                ),
+                checkpoint_file="checkpoint.pt",
+                data_name_or_path=_get_translate_path(
+                    self._model_name, _TH_EN_FILE_NAME, "vocab",
+                ),
+                bpe="sentencepiece",
+                sentencepiece_model=_get_translate_path(
+                    self._model_name, _TH_EN_FILE_NAME, "bpe", "spm.th.model",
+                ),
+            )
+    def _scb_en_th_translate(self, text: str) -> str:
+        self._scb_en_th_model_init()
+        tokens = " ".join(_en_tokenizer.tokenize(text))
+        translated = self._model.translate(tokens)
+        return translated.replace(' ', '').replace('▁', ' ').strip()
+    def _scb_th_en_model_init(self):
+        if self._model_name != "scb_1m_th-en_spm":
+            self._model_name = "scb_1m_th-en_spm"
+            _download_install(self._model_name)
+            self._model = TransformerModel.from_pretrained(
+                model_name_or_path=_get_translate_path(
+                    self._model_name, _TH_EN_FILE_NAME, "models",
+                ),
+                checkpoint_file="checkpoint.pt",
+                data_name_or_path=_get_translate_path(
+                    self._model_name, _TH_EN_FILE_NAME, "vocab",
+                ),
+                bpe="sentencepiece",
+                sentencepiece_model=_get_translate_path(
+                    self._model_name, _TH_EN_FILE_NAME, "bpe", "spm.th.model",
+                ),
+            )
+    def _scb_th_en_translate(self, text: str) -> str:
+        self._scb_th_en_model_init()
+        return self._model.translate(text)
+    def translate(self, text: str, source: str, target: str) -> str:
+        """
+        Translate Language
+
+        :param str text: input text in source language
+        :param str source: source language ("en" or "th")
+        :param str target: target language ("en" or "th")
+        
+        :return: translated text in target language
+        :rtype: str
+        """
+        translated = None
+        if source == "th" and target == "en":
+            translated = self._scb_th_en_translate(text)
+        elif source == "en" and target == "th":
+            translated = self._scb_en_th_translate(text)
+        else:
+            return ValueError("The combination of the arguments isn't allowed.")
+        return translated

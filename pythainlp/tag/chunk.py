4147ecc97663913b182e7bf20e6ac88e56002e74
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2021-01-17 21:58:08 +0700

    Update PEP8

--- pythainlp/tag/chunk.py
+++ pythainlp/tag/chunk.py
@@ -2,7 +2,10 @@
 from typing import Dict, List, Tuple
 
 
-def chunk_parse(sent: List[Tuple[str,str]], engine="crf", corpus="orchidpp") -> List[str]:
+def chunk_parse(
+    sent: List[Tuple[str, str]],
+    engine="crf", corpus="orchidpp"
+    ) -> List[str]:
     from .crfchunk import CRFchunk
     _engine = CRFchunk()
     return _engine.parse(sent)
--- pythainlp/tag/crfchunk.py
+++ pythainlp/tag/crfchunk.py
@@ -8,39 +8,39 @@ def _is_stopword(word: str) -> bool:  # เช็คว่าเป็นคำ�
     return word in thai_stopwords()
 
 
-def _doc2features(tokens: List[Tuple[str,str]], index: int) -> Dict:
+def _doc2features(tokens: List[Tuple[str, str]], index: int) -> Dict:
     """
     `tokens`  = a POS-tagged sentence [(w1, t1), ...]
     `index`   = the index of the token we want to extract features for
     """
     word, pos = tokens[index]
-    f =  {
+    f = {
         'word': word,
         'word_is_stopword': _is_stopword(word),
         'pos': pos,
     }
-    if index > 0 and index>1:
+    if index > 0 and index > 1:
         prevprevword, prevprevpos = tokens[index - 2]
-        f['prev-prev-word']= prevprevword
-        f['prev-prevz-word_is_stopword']= _is_stopword(prevprevword)
-        f['prev-prevz-pos']= prevprevpos
+        f['prev-prev-word'] = prevprevword
+        f['prev-prevz-word_is_stopword'] = _is_stopword(prevprevword)
+        f['prev-prevz-pos'] = prevprevpos
     if index > 0:
         prevword, prevpos = tokens[index-1]
-        f['prev-word']= prevword
-        f['prev-word_is_stopword']= _is_stopword(prevword)
-        f['prev-pos']= prevpos
+        f['prev-word'] = prevword
+        f['prev-word_is_stopword'] = _is_stopword(prevword)
+        f['prev-pos'] = prevpos
     else:
         f['BOS'] = True
-    if index< len(tokens)-2:
+    if index < len(tokens)-2:
         nextnextword, nextnextpos = tokens[index + 2]
-        f['nextnext-word']= nextnextword
-        f['nextnext-word_is_stopword']= _is_stopword(nextnextword)
-        f['nextnext-pos']= nextnextpos
-    if index< len(tokens)-1:
+        f['nextnext-word'] = nextnextword
+        f['nextnext-word_is_stopword'] = _is_stopword(nextnextword)
+        f['nextnext-pos'] = nextnextpos
+    if index < len(tokens)-1:
         nextword, nextpos = tokens[index+1]
-        f['next-word']= nextword
-        f['next-word_is_stopword']= _is_stopword(nextword)
-        f['next-pos']= nextpos
+        f['next-word'] = nextword
+        f['next-word_is_stopword'] = _is_stopword(nextword)
+        f['next-pos'] = nextpos
     else:
         f['EOS'] = True
 
@@ -48,18 +48,20 @@ def _doc2features(tokens: List[Tuple[str,str]], index: int) -> Dict:
 
 
 def extract_features(doc):
-    return [_doc2features(doc, i) for i in range(0,len(doc))]
+    return [_doc2features(doc, i) for i in range(0, len(doc))]
 
 
 class CRFchunk:
     def __init__(self, corpus: str = "orchidpp"):
         self.corpus = corpus
         self.load_model(self.corpus)
+
     def load_model(self, corpus: str):
         self.tagger = CRFTagger()
         if corpus == "orchidpp":
             self.path = path_pythainlp_corpus("crfchunk_orchidpp.model")
         self.tagger.open(self.path)
-    def parse(self, token_pos: List[Tuple[str,str]]):
+
+    def parse(self, token_pos: List[Tuple[str, str]]):
         self.xseq = extract_features(token_pos)
         return self.tagger.tag(self.xseq)

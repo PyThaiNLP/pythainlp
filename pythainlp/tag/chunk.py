e6e3dc2e7351e0252b9a51747a51d41e6f1845c7
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2021-01-17 21:59:21 +0700

    Update chunk.py

--- pythainlp/tag/chunk.py
+++ pythainlp/tag/chunk.py
@@ -5,7 +5,7 @@ from typing import Dict, List, Tuple
 def chunk_parse(
     sent: List[Tuple[str, str]],
     engine="crf", corpus="orchidpp"
-    ) -> List[str]:
+) -> List[str]:
     from .crfchunk import CRFchunk
     _engine = CRFchunk()
     return _engine.parse(sent)

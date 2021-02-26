ca81865d932d704a508b79ae8ef7f15bb75c8df7
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2021-02-26 20:47:09 +0700

    Update core.py

--- pythainlp/wangchanberta/core.py
+++ pythainlp/wangchanberta/core.py
@@ -60,7 +60,7 @@ class ThaiNameTagger:
         elif self.grouped_entities and self.dataset_name == "lst20":
             self.sent_ner = [(i['word'].replace("<_>", " "), i['entity_group'].replace('_','-').replace('E-','I-')) for i in self.json_ner]
         else:
-            self.sent_ner = [(i['word'].replace("<_>", " "), i['entity_group'].replace('_','-').replace('E-','I-')) for i in self.json_ner]
+            self.sent_ner = [(i['word'].replace("<_>", " "), i['entity'].replace('_','-').replace('E-','I-')) for i in self.json_ner]
 
         if tag:
             temp = ""

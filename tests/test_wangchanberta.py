8d1cbdbaddeb5a4675e2167ad7e81fad9f15b436
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2021-03-01 23:08:56 +0700

    Update test_wangchanberta.py

--- tests/test_wangchanberta.py
+++ tests/test_wangchanberta.py
@@ -16,10 +16,23 @@ class TestWangchanberta(unittest.TestCase):
             ner.get_ner("I คิด therefore I am ผ็ฎ์", tag = True)
         )
 
+        ner = ThaiNameTagger(grouped_entities=False)
+        self.assertIsNotNone(
+            ner.get_ner("I คิด therefore I am ผ็ฎ์", tag = True)
+        )
+
     def test_lst20_ner_wangchanberta(self):
         ner = ThaiNameTagger(dataset_name="lst20")
         self.assertIsNotNone(
             ner.get_ner("I คิด therefore I am ผ็ฎ์")
+        )
+        self.assertIsNotNone(
+            ner.get_ner("I คิด therefore I am ผ็ฎ์", tag = True)
+        )
+
+        ner = ThaiNameTagger(
+            dataset_name="lst20",
+            grouped_entities=False
         )
         self.assertIsNotNone(
             ner.get_ner("I คิด therefore I am ผ็ฎ์", tag = True)
@@ -31,4 +44,21 @@ class TestWangchanberta(unittest.TestCase):
         )
         self.assertIsNotNone(
             segment([])
+        )
+
+    def test_pos_tag_wangchanberta(self):
+        self.assertIsNotNone(
+            pos_tag("I คิด therefore I am ผ็ฎ์")
+        )
+        self.assertIsNotNone(
+            pos_tag(['I',' ','คิด',' ','therefore',' ','I',' ','am',' ','ผ็ฎ์'])
+        )
+        self.assertIsNotNone(
+            pos_tag(None)
+        )
+        self.assertIsNotNone(
+            pos_tag("I คิด therefore I am ผ็ฎ์",grouped_word=True)
+        )
+        self.assertIsNotNone(
+            pos_tag("ทดสอบระบบ",grouped_word=False)
         )

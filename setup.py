32df01b94e736014e173bd4b92923fdbd98889fc
root <root@DESKTOP-APEVDF3.localdomain>
2021-04-03 21:12:51 +0700

    Bump version: 2.3.1-beta0 → 2.3.1

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ extras = {
 
 setup(
     name="pythainlp",
-    version="2.3.1-beta0",
+    version="2.3.1",
     description="Thai Natural Language Processing library",
     long_description=readme,
     long_description_content_type="text/markdown",

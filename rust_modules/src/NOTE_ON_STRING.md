fe25187d9ac985ee9caaaf7ff79c71a8afe27e22
Thanathip Gorlph <gorlph@hotmail.com>
2021-05-06 23:52:00 +0700

    update dict_rader custom

---
+++ rust_modules/src/NOTE_ON_STRING.md
@@ -0,0 +1,16 @@
+# Why Use Handroll Bytes Slice As "CustomString" Instead of Rust String?
+
+Rust String (and &str) is actually a slice of valid UTF-8 bytes which is variable-length. It has no way of accessing a random index UTF-8 "character" with O(1) time complexity. 
+
+This means any algorithm with operations based on "character" index position will be horribly slow on Rust String.
+
+Hence; "fixed_bytes_str" which is transformed from a slice of valid UTF-8 bytes into a slice of 4-bytes length - padded left with 0.
+
+Consequently, regular expressions must be padded with \x00 for each unicode character to have 4 bytes
+
+Thai characters are 3-bytes length, so every thai char in regex is padded with \x00 one time.
+
+For "space" in regex, it is padded with \x00\x00\x00;
+
+
+More on UTF-8 (https://en.wikipedia.org/wiki/UTF-8)
--- rust_modules/src/tokenizer/dict_reader_custom.rs
+++ rust_modules/src/tokenizer/dict_reader_custom.rs
@@ -1,42 +1,20 @@
 use std::{ fs::{File, canonicalize}, path::PathBuf};
 use crate::fixed_bytes_str::four_bytes::CustomString;
-
+use rayon::prelude::*;
 use std::io::prelude::*;
 use std::io::BufReader;
 use super::trie_custom::Trie;
-const CARGO_PATH:&str = env!("CARGO_MANIFEST_DIR");
-const DEFAULT_DICT_PATH_RELATIVE_CARGO:&str = "pythainlp/corpus/words_th.txt"; 
+const DEFAULT_DICT_FILE:&str = include_str!("../../../pythainlp/corpus/words_th.txt");
 pub enum DictSource {
     WordList(Vec<String>),
     FilePath(PathBuf)
 }
 
 pub fn create_default_dict() -> Trie {
-
-
-    let dict_path = PathBuf::from(DEFAULT_DICT_PATH_RELATIVE_CARGO);
-    let cargo_path = PathBuf::from(CARGO_PATH);
-    let default_dict_path = cargo_path.join(dict_path);
-    let canon_path = canonicalize(&default_dict_path);
-   match canon_path {
-       Ok(canon_dict_pathbuf)=>{
-        let file = File::open(canon_dict_pathbuf).unwrap();
-
-        let mut reader = BufReader::new(file);
-        let mut line = String::with_capacity(50);
-        let mut dict:Vec<CustomString> = Vec::with_capacity(600);
-        while reader.read_line(&mut line).unwrap() != 0 {
-            dict.push(CustomString::new(&line));
-            line.clear();
-        }
-        dict.shrink_to_fit();
-        Trie::new(&dict) 
-       }
-       Err(unknown)=>{
-        panic!("{:?}",unknown);
-       }
-       
-    }
+    let default_dict = DEFAULT_DICT_FILE.par_lines().map(|word| {CustomString::new(word)}).collect::<Vec<CustomString>>();
+    
+   Trie::new(&default_dict)
+    
 
 }
 

21af5a795bb146cfad9e287289acf18906150b9b
Thanathip Gorlph <gorlph@hotmail.com>
2021-05-07 20:50:37 +0700

    fix incorrect function call

--- oxidized_thainlp/__init__.py
+++ oxidized_thainlp/__init__.py
@@ -1,6 +1,6 @@
 from .oxidized_thainlp import segment as rust_segment
 def segment(text:str, safe = False, parallel = False):
-    result = segment(text,True,True)
+    result = rust_segment(text,True,True)
     for (index,utf8bytes) in enumerate(result):
         result[index] = codecs.decode(bytearray(utf8bytes),encoding="utf-8")
     return result

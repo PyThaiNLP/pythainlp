35376a9de98f26dca442b75701a88ff00bc2cb68
Thanathip Gorlph <gorlph@hotmail.com>
2021-05-07 20:48:11 +0700

    fix typo of import

--- oxidized_thainlp/__init__.py
+++ oxidized_thainlp/__init__.py
@@ -1,4 +1,4 @@
-from oxidized_thainlp import segment as rust_segment
+from .oxidized_thainlp import segment as rust_segment
 def segment(text:str, safe = False, parallel = False):
     result = segment(text,True,True)
     for (index,utf8bytes) in enumerate(result):

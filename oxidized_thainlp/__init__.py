87e70607b737dfef2b52f2279c2699c4c6f731bf
Thanathip Gorlph <gorlph@hotmail.com>
2021-05-07 21:10:57 +0700

    ffdsfsdfsdf

--- oxidized_thainlp/__init__.py
+++ oxidized_thainlp/__init__.py
@@ -1,7 +1,7 @@
 from .oxidized_thainlp import segment as rust_segment
 import codecs
 def segment(text:str, safe = False, parallel = False):
-    result = rust_segment(text,True,True)
+    result = rust_segment(text,safe,parallel)
     for (index,utf8bytes) in enumerate(result):
         result[index] = codecs.decode(bytearray(utf8bytes),encoding="utf-8")
     return result

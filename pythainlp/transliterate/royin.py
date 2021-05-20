e3465917bc236c7352239d2e137c550319295292
Arthit Suriyawongkul <arthit@gmail.com>
2021-05-20 09:59:56 +0700

    Reorder condition in ifs, make it more consistent

--- pythainlp/transliterate/royin.py
+++ pythainlp/transliterate/royin.py
@@ -152,10 +152,11 @@ def _replace_consonants(word: str, consonants: str) -> str:
 
     if len(consonants) == 1:
         return word.replace(consonants[0], _CONSONANTS[consonants[0]][0])
+    len_cons = len(consonants)
 
     i = 0
-    len_cons = len(consonants)
     while i < len_cons:
+        len_word = len(word)
         if i == 0:
             if consonants[0] == _HO_HIP:
                 word = word.replace(consonants[0], "")
@@ -166,16 +167,16 @@ def _replace_consonants(word: str, consonants: str) -> str:
                     consonants[0], _CONSONANTS[consonants[0]][0]
                 )
                 i += 1
-        elif consonants[i] == _RO_RUA and i == len(word) and word[i - 1] == _RO_RUA:
+        elif i == len_word and consonants[i] == _RO_RUA and word[i - 1] == _RO_RUA:
             word = word.replace(
                     consonants[i], _CONSONANTS[consonants[i]][1]
             )
-        elif consonants[i] == _RO_RUA and i < len(word):
-            if i + 1 == len(word) and word[i] == _RO_RUA:
+        elif i < len_word and consonants[i] == _RO_RUA:
+            if i + 1 == len_word and word[i] == _RO_RUA:
                 word = word.replace(
                     consonants[i], _CONSONANTS[consonants[i]][1]
                 )
-            elif word[i] == _RO_RUA and i + 1 < len(word):
+            elif i + 1 < len_word and word[i] == _RO_RUA:
                 if word[i + 1] == _RO_RUA:
                     word = list(word)
                     del word[i + 1]

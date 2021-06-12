99774e250c87f62d68d45c96498227372a98a0ca
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2021-06-12 13:17:33 +0700

    Add test

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -10,18 +10,24 @@ class TestGeneratorPackage(unittest.TestCase):
     def test_unigram(self):
         _tnc_unigram = Unigram("tnc")
         self.assertIsNotNone(_tnc_unigram.gen_sentence("ผม"))
+        self.assertIsNotNone(_tnc_unigram.gen_sentence())
         _ttc_unigram = Unigram("ttc")
         self.assertIsNotNone(_ttc_unigram.gen_sentence("ผม"))
+        self.assertIsNotNone(_ttc_unigram.gen_sentence())
         _oscar_unigram = Unigram("oscar")
         self.assertIsNotNone(_oscar_unigram.gen_sentence("ผม"))
+        self.assertIsNotNone(_oscar_unigram.gen_sentence())
 
     def test_bigram(self):
         _bigram = Bigram()
         self.assertIsNotNone(_bigram.gen_sentence("ผม"))
+        self.assertIsNotNone(_bigram.gen_sentence())
 
     def test_tigram(self):
         _tigram = Tigram()
         self.assertIsNotNone(_tigram.gen_sentence("ผม"))
+        self.assertIsNotNone(_tigram.gen_sentence())
 
     def test_thai2fit(self):
         self.assertIsNotNone(gen_sentence("กาลครั้งหนึ่งนานมาแล้ว"))
+        self.assertIsNotNone(gen_sentence())

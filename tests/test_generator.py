6c64531f00f1ff79363c0c2cbe7f5dbfdab490bc
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2021-06-12 14:15:15 +0700

    Add test

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -10,23 +10,33 @@ class TestGeneratorPackage(unittest.TestCase):
     def test_unigram(self):
         _tnc_unigram = Unigram("tnc")
         self.assertIsNotNone(_tnc_unigram.gen_sentence("ผม"))
+        self.assertIsNotNone(_tnc_unigram.gen_sentence("ผม", output_str=False))
         self.assertIsNotNone(_tnc_unigram.gen_sentence())
+        self.assertIsNotNone(_tnc_unigram.gen_sentence(duplicate=True))
         _ttc_unigram = Unigram("ttc")
         self.assertIsNotNone(_ttc_unigram.gen_sentence("ผม"))
+        self.assertIsNotNone(_ttc_unigram.gen_sentence("ผม", output_str=False))
         self.assertIsNotNone(_ttc_unigram.gen_sentence())
+        self.assertIsNotNone(_ttc_unigram.gen_sentence(duplicate=True))
         _oscar_unigram = Unigram("oscar")
         self.assertIsNotNone(_oscar_unigram.gen_sentence("ผม"))
+        self.assertIsNotNone(_oscar_unigram.gen_sentence("ผม", output_str=False))
         self.assertIsNotNone(_oscar_unigram.gen_sentence())
+        self.assertIsNotNone(_oscar_unigram.gen_sentence(duplicate=True))
 
     def test_bigram(self):
         _bigram = Bigram()
         self.assertIsNotNone(_bigram.gen_sentence("ผม"))
+        self.assertIsNotNone(_bigram.gen_sentence("ผม", output_str=False))
         self.assertIsNotNone(_bigram.gen_sentence())
+        self.assertIsNotNone(_bigram.gen_sentence(duplicate=True))
 
     def test_tigram(self):
         _tigram = Tigram()
         self.assertIsNotNone(_tigram.gen_sentence("ผม"))
+        self.assertIsNotNone(_tigram.gen_sentence("ผม", output_str=False))
         self.assertIsNotNone(_tigram.gen_sentence())
+        self.assertIsNotNone(_tigram.gen_sentence(duplicate=True))
 
     def test_thai2fit(self):
         self.assertIsNotNone(gen_sentence("กาลครั้งหนึ่งนานมาแล้ว"))

a8a5a25afed2319c01c8e38e3f04fb74f1ca1808
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2021-06-16 18:00:58 +0700

    Create test_gpt.py

---
+++ tests/test_gpt.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from pythainlp.gpt.gpt_neo import FewShot
+
+
+class TestGPTackage(unittest.TestCase):
+    def test_gpt_neo_fewshot(self):
+        _model = FewShot('./model')
+        self.assertIsNotNone(_model)
+        _data= [
+            'txt: คนดี pos: +',
+            'txt: คนเลว pos: -'
+        ]
+        self.assertIsNone(_model.train(_data, './log', num_train_epochs=1))
+        self.assertIsNotNone(_model.gen('txt: คนชั่ว pos:'))
+        del _model
+        self.assertIsNotNone(FewShot('./model'))

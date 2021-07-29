7374cbbe727a6d6045073a1d76e699a092b2dd3f
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2021-07-29 14:39:19 +0700

    Update tltk.py

--- pythainlp/tag/tltk.py
+++ pythainlp/tag/tltk.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-from tltk.nlp import pos_tag_wordlist
+from tltk import nlp
 from typing import List, Tuple
 
+nlp.pos_load()
 
 def pos_tag(words: List[str], corpus: str = "tnc") -> List[Tuple[str, str]]:
     if corpus != "tnc":
         raise NotImplemented("tltk not support {0} corpus.".format(0))
-    return pos_tag_wordlist(words)
+    return nlp.pos_tag_wordlist(words)

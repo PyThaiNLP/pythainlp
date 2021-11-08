d61dcfe5166f5618122a25ef71637d5796d05686
Arthit Suriyawongkul <arthit@gmail.com>
2021-11-08 11:26:15 +0000

    Use nlpO3-python 1.2

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ extras = {
     ],
     "tltk": ["tltk>=1.3.8"],
     "oskut": ["oskut>=1.3"],
-    "nlpo3": ["nlpo3>=1.1.3"],
+    "nlpo3": ["nlpo3>=1.2.0"],
     "full": [
         "PyYAML>=5.3.1",
         "attacut>=1.0.4",
@@ -100,7 +100,7 @@ extras = {
         "symspellpy>=6.7.0",
         "tltk>=1.3.8",
         "oskut>=1.3",
-        "nlpo3>=1.1.3",
+        "nlpo3>=1.2.0",
     ],
 }
 

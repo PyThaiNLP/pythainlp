ee32b2e48acd47f1f1ff96482abf20f3d1818fc4
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2022-01-29 19:43:49 +0700

    Add load wordnet to tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,8 +6,11 @@ Each file in tests/ is for each main package.
 """
 import sys
 import unittest
+import nltk
 
 sys.path.append("../pythainlp")
+
+nltk.download('omw-1.4')  # load wordnet
 
 loader = unittest.TestLoader()
 testSuite = loader.discover("tests")

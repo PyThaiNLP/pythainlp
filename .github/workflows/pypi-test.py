ba452d0bea5174b79a6ddbd0a2b8143b06b03c5e
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2022-02-09 17:42:33 +0700

    Create pypi-test.py

---
+++ .github/workflows/pypi-test.py
@@ -0,0 +1,42 @@
+name: PyPI Unit test
+
+on:
+  push:
+    paths-ignore:
+      - '**.md'
+      - 'docs/**'
+
+jobs:
+  build:
+
+    runs-on: ubuntu-latest
+    strategy:
+      matrix:
+        python-version: [3.7]
+
+    steps:
+    - uses: actions/checkout@v2
+    - name: Set up Python ${{ matrix.python-version }}
+      uses: actions/setup-python@v1
+      with:
+        python-version: ${{ matrix.python-version }}
+    - name: Install dependencies
+      run: |
+        python -m pip install --upgrade pip
+        pip install pythainlp[full]
+        python -m nltk.downloader omw-1.4
+    - name: Test
+      run: |
+        mkdir pythainlp_test
+        cd pythainlp_test
+        pip download --no-binary=:all: --no-dependencies pythainlp
+        file="find . -name *.tar.gz"
+        file=$(eval "$file")
+        tar -xvzf $file --one-top-level
+        second="/"
+        path=${file//.tar.gz/$second}
+        cd $path
+        ls
+        python -m unittest discover
+        
+        

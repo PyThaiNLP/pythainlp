0a721e2e17d51fc09920c06ca051afcaa9e7e222
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2022-05-17 00:54:18 +0700

    Fixed #666 again

--- tests/test_tokenize.py
+++ tests/test_tokenize.py
@@ -610,9 +610,11 @@ class TestTokenizePackage(unittest.TestCase):
         self.assertEqual(nercut.segment(None), [])
         self.assertEqual(nercut.segment(""), [])
         self.assertIsNotNone(nercut.segment("ทดสอบ"))
-        self.assertIsNotNone(nercut.segment("ทุ๊กกโคนน"))
-        self.assertIsNotNone(nercut.segment("อือหือ"))
-        self.assertIsNotNone(nercut.segment("อย่าลืมอัพการ์ดนะจ๊ะ"))
+        self.assertEqual(nercut.segment("ทันแน่ๆ"), ['ทัน', 'แน่ๆ'])
+        self.assertEqual(nercut.segment("%1ครั้ง"), ['%', '1', 'ครั้ง'])
+        self.assertEqual(nercut.segment("ทุ๊กกโคนน"), ['ทุ๊กกโคนน'])
+        self.assertEqual(nercut.segment("อือหือ"), ['อือ', 'หือ'])
+        self.assertEqual(nercut.segment("อย่าลืมอัพการ์ดนะจ๊ะ"), ['อย่าลืมอัพการ์ดนะจ๊ะ'])
         self.assertIsNotNone(word_tokenize("ทดสอบ", engine="nercut"))
 
     def test_ssg(self):

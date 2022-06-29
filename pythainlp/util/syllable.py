359d76f518d06f8627e4bdd12a3ac5e24a4fb099
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2022-06-29 13:37:27 +0700

    Update syllable.py

--- pythainlp/util/syllable.py
+++ pythainlp/util/syllable.py
@@ -5,40 +5,45 @@ Syllable tools
 import re
 from pythainlp import thai_consonants
 
-spelling_class={
-    "กง":list("ง"),
-    "กม":list("ม"),
-    "เกย":list("ย"),
-    "เกอว":list("ว"),
-    "กน":list("นญณรลฬ"),
-    "กก":list("กขคฆ"),
-    "กด":list("ดจชซฎฏฐฑฒตถทธศษส"),
-    "กบ":list("บปภพฟ")
+spelling_class = {
+    "กง": list("ง"),
+    "กม": list("ม"),
+    "เกย": list("ย"),
+    "เกอว": list("ว"),
+    "กน": list("นญณรลฬ"),
+    "กก": list("กขคฆ"),
+    "กด": list("ดจชซฎฏฐฑฒตถทธศษส"),
+    "กบ": list("บปภพฟ")
 }
 
-thai_consonants_all=list(thai_consonants)
+thai_consonants_all = list(thai_consonants)
 thai_consonants_all.remove("อ")
 
-_temp = list("".join(["".join(spelling_class[i]) for i in spelling_class.keys()]))
-not_spelling_class =[j for j in thai_consonants_all if j not in _temp]
+_temp = list(
+    "".join(["".join(spelling_class[i]) for i in spelling_class.keys()])
+)
+not_spelling_class = [j for j in thai_consonants_all if j not in _temp]
 
 short = "ะัิึุ"
 re_short = re.compile("เ(.*)ะ|แ(.*)ะ|เ(.*)อะ|โ(.*)ะ|เ(.*)าะ", re.U)
-pattern = re.compile("เ(.*)า",re.U)
+pattern = re.compile("เ(.*)า", re.U)
+
+_check_1 = spelling_class["กง"]+spelling_class["กน"]+spelling_class["กม"]+spelling_class["เกย"]+spelling_class["เกอว"]
+_check_2 = spelling_class["กก"]+spelling_class["กบ"]+spelling_class["กด"]
 
 
 def sound_syllable(syllable:str) -> str:
-    consonants=[i for i in syllable if i in list(thai_consonants_all)]
+    consonants = [i for i in syllable if i in list(thai_consonants_all)]
     spelling_consonant = consonants[-1]
-    if len(syllable)<2:
+    if len(syllable) < 2:
         return "dead"
     elif (
         (
-            spelling_consonant in spelling_class["กก"]+spelling_class["กบ"]+spelling_class["กด"])
+            spelling_consonant in _check_2)
             and
             (
                 any((c in set("าีืแูาเโ")) for c in syllable) == False
-                and any((c in set("ำใไ")) for c in syllable)==False
+                and any((c in set("ำใไ")) for c in syllable) == False
                 and pattern.findall(syllable)!=True
             )
         ):
@@ -46,9 +51,9 @@ def sound_syllable(syllable:str) -> str:
     elif any((c in set("าีืแูาโ")) for c in syllable): # in syllable:
         if spelling_consonant != syllable[-1]:
             return "live"
-        elif spelling_consonant in spelling_class["กง"]+spelling_class["กน"]+spelling_class["กม"]+spelling_class["เกย"]+spelling_class["เกอว"]:
+        elif spelling_consonant in _check_1:
             return "live"
-        elif spelling_consonant in spelling_class["กก"]+spelling_class["กบ"]+spelling_class["กด"]:
+        elif spelling_consonant in _check_2:
             return "dead"
         elif (re_short.findall(syllable) or any((c in set(short)) for c in syllable)):
             return "dead"
@@ -57,15 +62,19 @@ def sound_syllable(syllable:str) -> str:
         return "live"
     elif pattern.findall(syllable):
         return "live"
-    elif spelling_consonant in spelling_class["กง"]+spelling_class["กน"]+spelling_class["กม"]+spelling_class["เกย"]+spelling_class["เกอว"]:
+    elif spelling_consonant in _check_1:
         if (
             re_short.findall(syllable)
             or
             any((c in set(short)) for c in syllable)
-        ) and len(consonants)<2:
+        ) and len(consonants) < 2:
             return "dead"
         return "live"
-    elif re_short.findall(syllable) or any((c in set(short)) for c in syllable):
+    elif (
+        re_short.findall(syllable)
+        or
+        any((c in set(short)) for c in syllable)
+    ):
         return "dead"
     else:
         return "dead"

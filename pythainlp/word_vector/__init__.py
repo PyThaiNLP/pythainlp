8ab14cc30fa66cfbad09a4cd0ff6e037ac64e847
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2022-07-20 02:44:35 +0700

    remove deprecated function in pythainlp.word_vector
    remove deprecated function in pythainlp.word_vector
    
    - get_model
    - doesnt_match
    - most_similar_cosmul
    - similarity
    - sentence_vectorizer
    
    use WordVector class instead

--- pythainlp/word_vector/__init__.py
+++ pythainlp/word_vector/__init__.py
@@ -5,19 +5,9 @@ thai2fit - Thai word vector.
 Initial code from https://github.com/cstorm125/thai2fit
 """
 __all__ = [
-    "doesnt_match",
-    "get_model",
-    "most_similar_cosmul",
-    "sentence_vectorizer",
-    "similarity",
     "WordVector",
 ]
 
 from pythainlp.word_vector.core import (
-    doesnt_match,
-    get_model,
-    most_similar_cosmul,
-    sentence_vectorizer,
-    similarity,
     WordVector,
 )

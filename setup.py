5d2d61b0333d7bcc022815c4e8959fa8e3755a8f
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2022-09-21 00:13:29 +0700

    PyThaiNLP v3.0.10
    - Fixed Wrong tag mapping from lst20 to UD #711

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ extras = {
 
 setup(
     name="pythainlp",
-    version="3.0.9",
+    version="3.0.10",
     description="Thai Natural Language Processing library",
     long_description=readme,
     long_description_content_type="text/markdown",

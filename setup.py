454ead407eb4564d61003693aa4bd06456d93abf
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2023-04-14 16:34:26 +0700

    PyThaiNLP 4.0

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 ﻿# -*- coding: utf-8 -*-
+# Copyright (C) 2016-2023 PyThaiNLP Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """
 Setup script for PyThaiNLP.
 
@@ -132,7 +145,7 @@ extras = {
 
 setup(
     name="pythainlp",
-    version="4.0.0beta1",
+    version="4.0.0",
     description="Thai Natural Language Processing library",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -165,7 +178,7 @@ setup(
         "Thai language",
     ],
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 3",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
@@ -181,7 +194,7 @@ setup(
         ],
     },
     project_urls={
-        "Documentation": "https://pythainlp.github.io/docs/3.0/",
+        "Documentation": "https://pythainlp.github.io/docs/4.0/",
         "Tutorials": "https://pythainlp.github.io/tutorials/",
         "Source Code": "https://github.com/PyThaiNLP/pythainlp",
         "Bug Tracker": "https://github.com/PyThaiNLP/pythainlp/issues",

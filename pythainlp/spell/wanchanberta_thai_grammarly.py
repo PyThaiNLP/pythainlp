11408f0db4e82793282242112f0e3b3ea55225c0
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2023-08-16 21:33:24 +0700

    Update wanchanberta_thai_grammarly.py

--- pythainlp/spell/wanchanberta_thai_grammarly.py
+++ pythainlp/spell/wanchanberta_thai_grammarly.py
@@ -12,6 +12,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""
+Two-stage Thai Misspelling Correction based on Pre-trained Language Models
+
+:See Also:
+    * Paper: \
+        https://ieeexplore.ieee.org/abstract/document/10202006
+    * GitHub: \
+        https://github.com/bookpanda/Two-stage-Thai-Misspelling-Correction-Based-on-Pre-trained-Language-Models
+"""
 from transformers import AutoModelForMaskedLM
 from transformers import AutoTokenizer, BertForTokenClassification
 import torch

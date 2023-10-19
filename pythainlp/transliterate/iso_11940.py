dbd2e6656d222a62b3e28b94033ad9ff726532ec
Arthit Suriyawongkul <arthit@gmail.com>
2023-10-19 14:32:39 +0100

    Fix code indent
    Co-authored-by: BLKSerene <blkserene@gmail.com>

--- pythainlp/transliterate/iso_11940.py
+++ pythainlp/transliterate/iso_11940.py
@@ -106,17 +106,17 @@ _tone_marks = {
 }
 
 _punctuation_and_digits = {
+    # ฯ can has two meanings in ISO 11940.
+    # If it is for abbrevation, it is paiyan noi.
+    # If it is for sentence termination, it is angkhan diao.
+    # Without semantic analysis, they cannot be distinguished from each other.
+    # In this simple implementation, we decided to always treat ฯ as paiyan noi.
+    # We commented out angkhan diao line to remove it from the dictionary
+    # and avoid having duplicate keys.
     "ๆ": "«",
     "ฯ": "ǂ",  # paiyan noi: U+01C2 ǂ Alveolar Click; ICU uses ‡ (double dagger)
     "๏": "§",
-# ฯ can has two meanings in ISO 11940.
-# If it is for abbrevation, it is paiyan noi.
-# If it is for sentence termination, it is angkhan diao.
-# Without semantic analysis, they cannot be distinguished from each other.
-# In this simple implementation, we decided to always treat ฯ as paiyan noi.
-# We commented out angkhan diao line below to remove it from the dictionary
-# to avoid duplice keys.
-#    "ฯ": "ǀ",  # angkhan diao: U+01C0 ǀ Dental Click; ICU uses | (vertical bar)
+    # "ฯ": "ǀ",  # angkhan diao: U+01C0 ǀ Dental Click; ICU uses | (vertical bar)
     "๚": "ǁ",  # angkhan khu: U+01C1 ǁ Lateral Click; ICU uses || (two vertical bars)
     "๛": "»",
     "๐": "0",

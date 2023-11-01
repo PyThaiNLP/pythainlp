801518c466f753ad72363b10c85ea72be2cfa57b
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2023-11-01 17:19:21 +0700

    Update newmm.py

--- pythainlp/tokenize/newmm.py
+++ pythainlp/tokenize/newmm.py
@@ -36,7 +36,8 @@ from pythainlp.util import Trie
 from pythainlp.tokenize.tcc_p import tcc_pos
 
 # match non-Thai tokens
-_PAT_NONTHAI = re.compile(r"[^\u0E00-\u0E7F]+")
+# number, (space,whitespace) and non-Thai tokens
+_PAT_NONTHAI = re.compile(r"\d+([,\.]\d+)*|[ \t]+|\r?\n|[^\u0E00-\u0E7F]+")
 
 # match 2-consonant Thai tokens
 _PAT_THAI_TWOCHARS = re.compile("[ก-ฮ]{,2}$")

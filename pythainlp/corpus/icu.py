171214c7fee1fcdbd96f1afa664b8a4e6c24bf45
Arthit Suriyawongkul <arthit@gmail.com>
2023-12-06 10:12:42 +0000

    Update and rename thai_icu.py to icu.py
    Also rename `thai_icu()` to `thai_icu_words()` to make it more explicit and consistent with others, like: `thai_orst_words()`

---
+++ pythainlp/corpus/icu.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: Copyright 2016-2023 PyThaiNLP Project
+# SPDX-License-Identifier: Apache-2.0
+"""
+Provides an optional word list from International Components for Unicode (ICU) dictionary.
+"""
+from typing import FrozenSet
+
+from pythainlp.corpus.common import get_corpus
+
+
+_THAI_ICU_FILENAME = "icubrk_th.txt"
+
+
+def thai_icu_words() -> FrozenSet[str]:
+    """
+    Return a frozenset of words from the Thai dictionary for BreakIterator of the
+    International Components for Unicode (ICU).
+
+    :return: :class:`frozenset` containing `str`
+    :rtype: :class:`frozenset`
+    """
+
+    _WORDS = get_corpus(_THAI_ICU_FILENAME, comments=False)
+
+    return _WORDS

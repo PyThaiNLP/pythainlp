c8d643f2eb00015f044041b8469a0f40d3482f85
Arthit Suriyawongkul <arthit@gmail.com>
2023-12-07 02:05:07 +0000

    Make docstring within line length limit

--- pythainlp/coref/core.py
+++ pythainlp/coref/core.py
@@ -14,12 +14,14 @@ def coreference_resolution(
 
     :param List[str] texts: list of texts to apply coreference resolution to
     :param str model_name: coreference resolution model
-    :param str device: device for running coreference resolution model on ("cpu", "cuda", and others)
+    :param str device: device for running coreference resolution model on\
+        ("cpu", "cuda", and others)
     :return: List of texts with coreference resolution
     :rtype: List[dict]
 
     :Options for model_name:
-        * *han-coref-v1.0* - (default) Han-Corf: Thai coreference resolution by PyThaiNLP v1.0
+        * *han-coref-v1.0* - (default) Han-Coref: Thai coreference resolution\
+            by PyThaiNLP v1.0
 
     :Example:
     ::

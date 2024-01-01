3d324e3de50db16b14aafeed267080722cef87b3
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2024-01-01 17:43:22 +0700

    Fixed pep8

--- pythainlp/morpheme/word_formation.py
+++ pythainlp/morpheme/word_formation.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 # SPDX-FileCopyrightText: Copyright 2016-2024 PyThaiNLP Project
 # SPDX-License-Identifier: Apache-2.0
-from pythainlp.transliterate import pronunciate
 from pythainlp import thai_consonants
 
 
-def nighit(w1: str,w2: str)->str:
+def nighit(w1: str, w2: str) -> str:
     """
     Nighit (นิคหิต or  ํ ) is the niggahita in Thai language for create new \
     words from Pali language in Thai.
@@ -29,7 +28,7 @@ def nighit(w1: str,w2: str)->str:
         assert nighit("สํ","ปทา")=="สัมปทา"
         assert nighit("สํ","โยค")=="สังโยค"
     """
-    if not str(w1).endswith('ํ') and len(w1)!=2:
+    if not str(w1).endswith('ํ') and len(w1) != 2:
         raise NotImplementedError(f"The function doesn't support {w1}.")
     list_w1 = list(w1)
     list_w2 = list(w2)
@@ -37,17 +36,17 @@ def nighit(w1: str,w2: str)->str:
     newword.append(list_w1[0])
     newword.append("ั")
     consonant_start = [i for i in list_w2 if i in set(thai_consonants)][0]
-    if consonant_start in ["ก","ช","ค","ข","ง"]:
+    if consonant_start in ["ก", "ช", "ค", "ข", "ง"]:
         newword.append("ง")
-    elif consonant_start in ["จ","ฉ","ช","ฌ"]:
+    elif consonant_start in ["จ", "ฉ", "ช", "ฌ"]:
         newword.append("ญ")
-    elif consonant_start in ["ฎ","ฐ","ฑ","ณ"]:
+    elif consonant_start in ["ฎ", "ฐ", "ฑ", "ณ"]:
         newword.append("ณ")
-    elif consonant_start in ["ด","ถ","ท","ธ","น"]:
+    elif consonant_start in ["ด", "ถ", "ท", "ธ", "น"]:
         newword.append("น")
-    elif consonant_start in ["ป","ผ","พ","ภ"]:
+    elif consonant_start in ["ป", "ผ", "พ", "ภ"]:
         newword.append("ม")
-    elif consonant_start in ["ย","ร","ล","ฬ","ว","ศ","ษ","ส","ห"]:
+    elif consonant_start in ["ย", "ร", "ล", "ฬ", "ว", "ศ", "ษ", "ส", "ห"]:
         newword.append("ง")
     else:
         raise NotImplementedError(f"""
--- tests/test_morpheme.py
+++ tests/test_morpheme.py
@@ -8,9 +8,9 @@ from pythainlp.morpheme import nighit
 
 class TestMorphemePackage(unittest.TestCase):
     def test_nighit(self):
-        self.assertEqual(nighit("สํ","คีต"), "สังคีต")
-        self.assertEqual(nighit("สํ","จร"), "สัญจร")
-        self.assertEqual(nighit("สํ","ฐาน"), "สัณฐาน")
-        self.assertEqual(nighit("สํ","นิษฐาน"), "สันนิษฐาน")
-        self.assertEqual(nighit("สํ","ปทา"), "สัมปทา")
-        self.assertEqual(nighit("สํ","โยค"), "สังโยค")
+        self.assertEqual(nighit("สํ", "คีต"), "สังคีต")
+        self.assertEqual(nighit("สํ", "จร"), "สัญจร")
+        self.assertEqual(nighit("สํ", "ฐาน"), "สัณฐาน")
+        self.assertEqual(nighit("สํ", "นิษฐาน"), "สันนิษฐาน")
+        self.assertEqual(nighit("สํ", "ปทา"), "สัมปทา")
+        self.assertEqual(nighit("สํ", "โยค"), "สังโยค")

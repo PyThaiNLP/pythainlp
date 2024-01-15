d771fd00e69115ca3a3e3824330fc48c3ce78570
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2024-01-15 14:49:21 +0700

    PyThaiNLP 5.0.0dev2

--- setup.py
+++ setup.py
@@ -161,7 +161,7 @@ extras = {
 
 setup(
     name="pythainlp",
-    version="5.0.0dev1",
+    version="5.0.0dev2",
     description="Thai Natural Language Processing library",
     long_description=readme,
     long_description_content_type="text/markdown",

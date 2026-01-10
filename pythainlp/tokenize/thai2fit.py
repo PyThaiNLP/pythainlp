f31c0110cc5c71865d1b5a16b50d40fda352b40b
what_in_the_nim <chatcharinsang@gmail.com>
2026-01-10 14:43:07 +0700

    fix: add license header to thai2fit.py

--- pythainlp/tokenize/thai2fit.py
+++ pythainlp/tokenize/thai2fit.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: 2016-2026 PyThaiNLP Project
+# SPDX-FileType: SOURCE
+# SPDX-License-Identifier: Apache-2.0
+
 from functools import lru_cache
 from pythainlp.corpus import get_corpus
 from pythainlp.tokenize import Tokenizer

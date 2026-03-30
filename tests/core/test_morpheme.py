14c8a1d23b4523e8a3c36802304453cfb07eec4e
Arthit Suriyawongkul <arthit@gmail.com>
2026-03-30 23:33:48 +0100

    Remove second args of assertFalse/assertTrue

--- tests/core/test_morpheme.py
+++ tests/core/test_morpheme.py
@@ -26,7 +26,12 @@ class MorphemeTestCase(unittest.TestCase):
         self.assertFalse(is_native_thai("116"), False)
         self.assertFalse(is_native_thai("abc"), False)
         self.assertFalse(is_native_thai("ศา"), False)
-        self.assertFalse(is_native_thai("ลักษ์"), False)
+self.assertFalse(is_native_thai(None))  # type: ignore[arg-type]
+self.assertFalse(is_native_thai(""))
+self.assertFalse(is_native_thai("116"))
+self.assertFalse(is_native_thai("abc"))
+self.assertFalse(is_native_thai("ศา"))
+self.assertFalse(is_native_thai("ลักษ์"))
         self.assertFalse(is_native_thai("มาร์ค"))
         self.assertFalse(is_native_thai("เลข"))
         self.assertFalse(is_native_thai("เทเวศน์"))
